0279d87f90fefc4c7e809afc8e5dcb6673285b3c
Allen Goodman <allen.goodman@icloud.com>
2020-10-16 21:41:13 -0400

    ClassifierSettings

--- src/ClassifierList.tsx
+++ src/ClassifierList.tsx
@@ -1,4 +1,3 @@
-import { CollapsibleList } from "./CollapsibleList";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import AddIcon from "@material-ui/icons/Add";
@@ -13,57 +12,81 @@ import Collapse from "@material-ui/core/Collapse";
 import { ListItemSecondaryAction } from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import SettingsIcon from "@material-ui/icons/Settings";
+import { ClassifierSettingsDialog } from "./ClassifierSettingsDialog";
+import Tooltip from "@material-ui/core/Tooltip";
 
 export const ClassifierList = () => {
   const [collapsed, setCollapsed] = React.useState(true);
 
-  const onClick = () => {
+  const onCollapseClick = () => {
     setCollapsed(!collapsed);
   };
 
+  const [
+    openClassifierSettingsDialog,
+    setOpenClassifierSettingsDialog,
+  ] = React.useState(false);
+
+  const onOpenClassifierSettingsDialog = () => {
+    setOpenClassifierSettingsDialog(true);
+  };
+
+  const onCloseClassifierSettingsDialog = () => {
+    setOpenClassifierSettingsDialog(false);
+  };
+
   return (
-    <List dense>
-      <ListItem button dense onClick={onClick}>
-        <ListItemIcon>
-          {collapsed ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-        </ListItemIcon>
+    <React.Fragment>
+      <List dense>
+        <ListItem button dense onClick={onCollapseClick}>
+          <ListItemIcon>
+            {collapsed ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          </ListItemIcon>
 
-        <ListItemText primary="Classifier" />
+          <ListItemText primary="Classifier" />
 
-        <ListItemSecondaryAction>
-          <IconButton edge="end">
-            <SettingsIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
+          <ListItemSecondaryAction>
+            <Tooltip title="Classifier settings">
+              <IconButton edge="end" onClick={onOpenClassifierSettingsDialog}>
+                <SettingsIcon />
+              </IconButton>
+            </Tooltip>
+          </ListItemSecondaryAction>
+        </ListItem>
 
-      <Collapse in={collapsed} timeout="auto" unmountOnExit>
-        <List component="div" dense disablePadding>
-          <ListItem button disabled>
-            <ListItemIcon>
-              <AddIcon />
-            </ListItemIcon>
+        <Collapse in={collapsed} timeout="auto" unmountOnExit>
+          <List component="div" dense disablePadding>
+            <ListItem button disabled>
+              <ListItemIcon>
+                <AddIcon />
+              </ListItemIcon>
 
-            <ListItemText primary="Fit" />
-          </ListItem>
+              <ListItemText primary="Fit" />
+            </ListItem>
 
-          <ListItem button disabled>
-            <ListItemIcon>
-              <FolderOpenIcon />
-            </ListItemIcon>
+            <ListItem button disabled>
+              <ListItemIcon>
+                <FolderOpenIcon />
+              </ListItemIcon>
 
-            <ListItemText primary="Evaluate" />
-          </ListItem>
+              <ListItemText primary="Evaluate" />
+            </ListItem>
 
-          <ListItem button disabled>
-            <ListItemIcon>
-              <SaveIcon />
-            </ListItemIcon>
+            <ListItem button disabled>
+              <ListItemIcon>
+                <SaveIcon />
+              </ListItemIcon>
 
-            <ListItemText primary="Predict" />
-          </ListItem>
-        </List>
-      </Collapse>
-    </List>
+              <ListItemText primary="Predict" />
+            </ListItem>
+          </List>
+        </Collapse>
+      </List>
+
+      <ClassifierSettingsDialog
+        onClose={onCloseClassifierSettingsDialog}
+        open={openClassifierSettingsDialog}
+      />
+    </React.Fragment>
   );
 };
---
+++ src/ClassifierSettingsDialog.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Dialog from "@material-ui/core/Dialog";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import { TransitionProps } from "@material-ui/core/transitions";
+import Slide from "@material-ui/core/Slide";
+import { useStyles } from "./index.css";
+
+type ClassifierSettingsDialogProps = {
+  onClose: () => void;
+  open: boolean;
+};
+
+const DialogTransition = React.forwardRef(
+  (
+    props: TransitionProps & { children?: React.ReactElement },
+    ref: React.Ref<unknown>
+  ) => {
+    return <Slide direction="right" ref={ref} {...props} />;
+  }
+);
+
+export const ClassifierSettingsDialog = ({
+  onClose,
+  open,
+}: ClassifierSettingsDialogProps) => {
+  const classes = useStyles();
+
+  return (
+    <Dialog
+      fullScreen
+      onClose={onClose}
+      open={open}
+      TransitionComponent={DialogTransition}
+    >
+      <AppBar className={classes.settingsDialogAppBar}>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Dialog>
+  );
+};
