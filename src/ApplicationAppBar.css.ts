1e24cf84c1757c43f4f8a06b07f2e17159de6d6b
Allen Goodman <allen.goodman@icloud.com>
2020-10-22 13:44:43 -0400

    AppBar

---
+++ src/ApplicationAppBar.css.ts
@@ -0,0 +1,68 @@
+import { fade, makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      duration: theme.transitions.duration.leavingScreen,
+      easing: theme.transitions.easing.sharp,
+    }),
+  },
+  appBarShift: {
+    marginLeft: theme.spacing(30),
+    transition: theme.transitions.create(["margin", "width"], {
+      duration: theme.transitions.duration.enteringScreen,
+      easing: theme.transitions.easing.easeOut,
+    }),
+    width: `calc(100% - ${theme.spacing(30)}px)`,
+  },
+  button: {
+    margin: theme.spacing(1),
+    textTransform: "none",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "#f1f3f4",
+    "&:hover": {
+      backgroundColor: "#f1f3f4",
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    alignItems: "center",
+    display: "flex",
+    height: "100%",
+    justifyContent: "center",
+    padding: theme.spacing(0, 2),
+    pointerEvents: "none",
+    position: "absolute",
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+}));
--- src/ApplicationAppBar.tsx
+++ src/ApplicationAppBar.tsx
@@ -5,7 +5,11 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Typography from "@material-ui/core/Typography";
 import React from "react";
-import { useStyles } from "./index.css";
+import { useStyles } from "./ApplicationAppBar.css";
+import { Button } from "@material-ui/core";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
 
 type ApplicationAppBar = {
   onOpenDrawer: () => void;
@@ -19,19 +23,48 @@ export const ApplicationAppBar = ({
   const classes = useStyles();
 
   return (
-    <AppBar
-      className={clsx(classes.appBar, { [classes.appBarShift]: openDrawer })}
-      position="fixed"
-    >
-      <Toolbar>
-        <IconButton color="inherit" onClick={onOpenDrawer} edge="start">
-          <MenuIcon />
-        </IconButton>
+    <div className={classes.grow}>
+      <AppBar
+        className={clsx(classes.appBar, { [classes.appBarShift]: openDrawer })}
+        color="transparent"
+        position="fixed"
+      >
+        <Toolbar>
+          <IconButton color="inherit" onClick={onOpenDrawer} edge="start">
+            <MenuIcon />
+          </IconButton>
 
-        <Typography color="inherit" noWrap variant="h6">
-          Piximi
-        </Typography>
-      </Toolbar>
-    </AppBar>
+          <Typography
+            className={classes.title}
+            color="inherit"
+            noWrap
+            variant="h6"
+          >
+            Piximi
+          </Typography>
+
+          <div className={classes.grow} />
+
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              disabled={true}
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+
+          <Button className={classes.button} startIcon={<CloudUploadIcon />}>
+            Open images
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
