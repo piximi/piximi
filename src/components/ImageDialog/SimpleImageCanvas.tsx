959044d87a69606768b215c1f8ef7a7cf788fd63
alucas <alicelucas93@gmail.com>
2020-11-21 10:49:03 -0500

    clean up

--- src/components/ImageDialog/SimpleImageCanvas.tsx
+++ src/components/ImageDialog/SimpleImageCanvas.tsx
@@ -20,14 +20,11 @@ export const SimpleImageCanvas = ({
   brush,
 }: ImageCanvasProps) => {
   const classes = useStyles();
-
-  //const [clicks, setClick] = useState<Array<clickData>>([]);
   const [click, setNewClick] = useState<clickData>({
     x: 0,
     y: 0,
     dragging: false,
   });
-  //const [paint, setPaint] = useState<boolean>(false);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const drawLine = (
@@ -46,23 +43,6 @@ export const SimpleImageCanvas = ({
     context.closePath();
   };
 
-  // const clearCanvas = (ctx: CanvasRenderingContext2D) => {
-  //   setClick([]);
-  //   ctx.clearRect(0, 0, 300, 300);
-  // };
-
-  // const redraw = (ctx: CanvasRenderingContext2D) => {
-  //   ctx.beginPath();
-  //   clicks.forEach((click, index) => {
-  //     clicks[index].dragging
-  //       ? ctx.moveTo(clicks[index - 1].x, clicks[index - 1].y)
-  //       : ctx.moveTo(clicks[index].x, clicks[index].y);
-  //     ctx.lineTo(clicks[index].x, clicks[index].y);
-  //     ctx.stroke();
-  //   });
-  //   ctx.closePath();
-  // };
-
   const onMouseDown = (
     event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
   ) => {
@@ -77,90 +57,39 @@ export const SimpleImageCanvas = ({
   const onMouseMove = (
     event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
   ) => {
-    if (canvasRef.current) {
+    if (click.dragging && canvasRef.current) {
       const rect = canvasRef.current.getBoundingClientRect();
       const clickX = event.clientX - rect.left;
       const clickY = event.clientY - rect.top;
       const context = canvasRef.current.getContext("2d");
-      if (click.dragging) {
-        if (context) {
-          drawLine(context, click.x, click.y, clickX, clickY);
-        }
-        setNewClick({ x: clickX, y: clickY, dragging: true });
-
-        event.preventDefault();
+      if (context) {
+        drawLine(context, click.x, click.y, clickX, clickY);
       }
+      setNewClick({ x: clickX, y: clickY, dragging: true });
+      event.preventDefault();
     }
   };
 
   const onMouseUp = (
     event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
   ) => {
-    if (click.dragging) {
-      if (canvasRef.current) {
-        const rect = canvasRef.current.getBoundingClientRect();
-        const clickX = event.clientX - rect.left;
-        const clickY = event.clientY - rect.top;
-        const context = canvasRef.current.getContext("2d");
-        if (context) {
-          drawLine(context, click.x, click.y, clickX, clickY);
-        }
+    if (click.dragging && canvasRef.current) {
+      const rect = canvasRef.current.getBoundingClientRect();
+      const clickX = event.clientX - rect.left;
+      const clickY = event.clientY - rect.top;
+      const context = canvasRef.current.getContext("2d");
+      if (context) {
+        drawLine(context, click.x, click.y, clickX, clickY);
       }
-      setNewClick({ x: 0, y: 0, dragging: false });
     }
+    setNewClick({ x: 0, y: 0, dragging: false });
   };
 
-  // const onMouseDown = (
-  //   event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
-  // ) => {
-  //   if (canvasRef.current) {
-  //     const rect = canvasRef.current.getBoundingClientRect();
-  //     const clickX = event.clientX - rect.left;
-  //     const clickY = event.clientY - rect.top;
-  //     const newClick = { x: clickX, y: clickY, dragging: false };
-  //     setClick([...clicks, newClick]);
-  //     const context = canvasRef.current.getContext("2d");
-  //     if (context) {
-  //       redraw(context);
-  //     }
-  //   }
-  //   setPaint(true);
-  // };
-  //
-  // const onMouseMove = (
-  //   event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
-  // ) => {
-  //   if (canvasRef.current) {
-  //     const rect = canvasRef.current.getBoundingClientRect();
-  //     const clickX = event.clientX - rect.left;
-  //     const clickY = event.clientY - rect.top;
-  //     if (paint) {
-  //       const newClick = { x: clickX, y: clickY, dragging: true };
-  //       setClick([...clicks, newClick]);
-  //       const context = canvasRef.current.getContext("2d");
-  //       if (context) {
-  //         redraw(context);
-  //       }
-  //     }
-  //   }
-  //   event.preventDefault();
-  // };
-
-  // const onMouseUp = (
-  //   event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
-  // ) => {
-  //   setPaint(false);
-  //   const context = canvasRef.current?.getContext("2d");
-  //   if (context) {
-  //     redraw(context);
-  //   }
-  // };
-
-  // const onMouseOut = (
-  //   event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
-  // ) => {
-  //   setPaint(false);
-  // };
+  const onMouseOut = (
+    event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
+  ) => {
+    setNewClick({ x: 0, y: 0, dragging: false });
+  };
 
   return (
     <div>
@@ -169,7 +98,7 @@ export const SimpleImageCanvas = ({
           onMouseDown={onMouseDown}
           onMouseMove={onMouseMove}
           onMouseUp={onMouseUp}
-          //onMouseOut={onMouseOut}
+          onMouseOut={onMouseOut}
           ref={canvasRef}
           id={"myCanvas"}
           width={300}
