5375c81c2268b7dfaace94a47d71efc2ad383a9c
Allen Goodman <allen.goodman@icloud.com>
2021-01-03 18:20:29 -0500

    ButtonGroupMenu

---
+++ src/components/ImageViewer/ImageViewerAppBar/AppBarToolbar/ButtonGroupMenu/ButtonGroupMenu.tsx
@@ -0,0 +1,46 @@
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import MenuList from "@material-ui/core/MenuList";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import React from "react";
+
+type ButtonGroupPopperProps = {
+  anchorEl: any;
+  children: React.ReactNode;
+  onClose: () => void;
+  open: boolean;
+};
+
+export const ButtonGroupMenu = ({
+  anchorEl,
+  children,
+  onClose,
+  open,
+}: ButtonGroupPopperProps) => {
+  return (
+    <Popper
+      anchorEl={anchorEl.current}
+      disablePortal
+      open={open}
+      role={undefined}
+      transition
+    >
+      {({ TransitionProps, placement }) => (
+        <Grow
+          {...TransitionProps}
+          style={{
+            transformOrigin:
+              placement === "bottom" ? "center top" : "center bottom",
+          }}
+        >
+          <Paper>
+            <ClickAwayListener onClickAway={onClose}>
+              <MenuList dense>{children}</MenuList>
+            </ClickAwayListener>
+          </Paper>
+        </Grow>
+      )}
+    </Popper>
+  );
+};
