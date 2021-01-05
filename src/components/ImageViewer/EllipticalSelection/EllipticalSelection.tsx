7d5f2990343488a91ddf442d7b553686f17bd3f3
alucas <alicelucas93@gmail.com>
2021-01-05 15:09:16 -0500

    Konda elliptical select

--- src/components/ImageViewer/EllipticalSelection/EllipticalSelection.tsx
+++ src/components/ImageViewer/EllipticalSelection/EllipticalSelection.tsx
@@ -13,6 +13,11 @@ export const EllipticalSelection = ({ data }: ImageViewerProps) => {
 
   const stage = React.useRef<Stage>(null);
 
+  const [x, setX] = React.useState<number>(0);
+  const [y, setY] = React.useState<number>(0);
+  const [radiusX, setRadiusX] = React.useState<number>(50);
+  const [radiusY, setRadiusY] = React.useState<number>(50);
+
   const onMouseDown = () => {};
 
   const onMouseMove = () => {};
@@ -31,6 +36,13 @@ export const EllipticalSelection = ({ data }: ImageViewerProps) => {
     >
       <ReactKonva.Layer>
         <ReactKonva.Image image={image} />
+        <ReactKonva.Ellipse
+          x={x}
+          y={y}
+          radiusX={radiusX}
+          radiusY={radiusY}
+          stroke="white"
+        />
       </ReactKonva.Layer>
     </ReactKonva.Stage>
   );
