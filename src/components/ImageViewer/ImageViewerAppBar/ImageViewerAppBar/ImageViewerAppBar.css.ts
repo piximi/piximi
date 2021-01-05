e3a389cdead0df454a9015296f3bb4a680f33c5c
Allen Goodman <allen.goodman@icloud.com>
2021-01-05 13:23:42 -0500

    settings

--- src/components/ImageViewer/ImageViewerAppBar/ImageViewerAppBar/ImageViewerAppBar.css.ts
+++ src/components/ImageViewer/ImageViewerAppBar/ImageViewerAppBar/ImageViewerAppBar.css.ts
@@ -1,10 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const drawerWidth = 80 + 240;
-
 export const useStyles = makeStyles((theme) => ({
   appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginRight: drawerWidth,
+    zIndex: theme.zIndex.drawer + 1,
   },
 }));
