af8dc8749b483346edba768727d53a238443347d
Allen Goodman <allen.goodman@icloud.com>
2021-01-04 21:35:39 -0500

    ToolbarButtonGroup

--- src/components/ImageViewer/ImageViewerAppBar/AppBarToolbar/ToolbarButtonGroup/ToolbarButtonGroup.tsx
+++ src/components/ImageViewer/ImageViewerAppBar/AppBarToolbar/ToolbarButtonGroup/ToolbarButtonGroup.tsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 import { Image } from "../../../../../types/Image";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import Button from "@material-ui/core/Button";
 import SvgIcon from "@material-ui/core/SvgIcon";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import { ButtonGroupMenu } from "../ButtonGroupMenu";
-import { ButtonGroupMenuItem } from "../ButtonGroupMenuItem";
 import { ReactComponent as RectangularIcon } from "../../../../../icons/Rectangular.svg";
 import { SelectionMethod } from "../../../../../types/SelectionMethod";
 import { ReactComponent as EllipticalIcon } from "../../../../../icons/Elliptical.svg";
@@ -13,214 +8,72 @@ import { ReactComponent as LassoIcon } from "../../../../../icons/Lasso.svg";
 import { ReactComponent as MagneticIcon } from "../../../../../icons/Magnetic.svg";
 import { ReactComponent as MagicWandIcon } from "../../../../../icons/MagicWand.svg";
 import { ReactComponent as QuickIcon } from "../../../../../icons/Quick.svg";
-import { useMenu } from "../../../../../hooks";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup/ToggleButtonGroup";
+import ToggleButton from "@material-ui/lab/ToggleButton/ToggleButton";
+import Tooltip from "@material-ui/core/Tooltip";
 
 type ToolbarButtonGroupProps = {
   data: Image;
 };
 
 export const ToolbarButtonGroup = ({ data }: ToolbarButtonGroupProps) => {
-  const {
-    anchorEl: marqueeMenuAnchorEl,
-    onClose: onCloseMarqueeMenu,
-    onOpen: onOpenMarqueeMenu,
-    open: openMarqueeMenu,
-  } = useMenu();
-
-  const {
-    anchorEl: lassoMenuAnchorEl,
-    onClose: onCloseLassoMenu,
-    onOpen: onOpenLassoMenu,
-    open: openLassoMenu,
-  } = useMenu();
-
-  const {
-    anchorEl: probabilisticMenuAnchorEl,
-    onClose: onCloseProbabilisticMenu,
-    onOpen: onOpenProbabilisticMenu,
-    open: openProbabilisticMenu,
-  } = useMenu();
+  const onChange = (
+    event: React.MouseEvent<HTMLElement>,
+    method: SelectionMethod
+  ) => {
+    setMethod(method);
+  };
 
   const [method, setMethod] = useState<SelectionMethod>(
     SelectionMethod.Rectangular
   );
 
-  const [visibleLassoMethod, setVisibleLassoMethod] = useState<SelectionMethod>(
-    SelectionMethod.Lasso
-  );
-
-  const [visibleMarqueeIcon, setVisibleMarqueeIcon] = useState<SelectionMethod>(
-    SelectionMethod.Rectangular
-  );
-
-  const [
-    visibleProbabilisticIcon,
-    setVisibleProbabilisticIcon,
-  ] = useState<SelectionMethod>(SelectionMethod.Quick);
-
-  const onClickLassoMenuItem = (
-    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
-    method: SelectionMethod
-  ) => {
-    setVisibleLassoMethod(method);
-
-    onCloseLassoMenu(event);
-  };
-
-  const onClickMarqueeMenuItem = (
-    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
-    method: SelectionMethod
-  ) => {
-    setVisibleMarqueeIcon(method);
-
-    onCloseMarqueeMenu(event);
-  };
-
-  const onClickProbabilisticMenuItem = (
-    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
-    method: SelectionMethod
-  ) => {
-    setVisibleProbabilisticIcon(method);
-
-    onCloseProbabilisticMenu(event);
-  };
-
-  const MarqueeMethodIcon = () => {
-    switch (visibleMarqueeIcon) {
-      case SelectionMethod.Elliptical:
-        return <EllipticalIcon />;
-      default:
-        return <RectangularIcon />;
-    }
-  };
-
-  const LassoMethodIcon = () => {
-    switch (visibleLassoMethod) {
-      case SelectionMethod.Magnetic:
-        return <MagneticIcon />;
-      default:
-        return <LassoIcon />;
-    }
-  };
-
-  const ProbabilisticMethodIcon = () => {
-    switch (visibleProbabilisticIcon) {
-      case SelectionMethod.Color:
-        return <MagicWandIcon />;
-      default:
-        return <QuickIcon />;
-    }
-  };
-
   return (
-    <React.Fragment>
-      <ButtonGroup
-        color="inherit"
-        ref={marqueeMenuAnchorEl}
-        variant="contained"
+    <ToggleButtonGroup exclusive onChange={onChange} value={method}>
+      <ToggleButton
+        aria-label="rectangular selection"
+        value={SelectionMethod.Rectangular}
       >
-        <Button>
-          <SvgIcon fontSize="small">
-            <MarqueeMethodIcon />
-          </SvgIcon>
-        </Button>
+        <SvgIcon fontSize="small">
+          <RectangularIcon />
+        </SvgIcon>
+      </ToggleButton>
 
-        <Button color="inherit" onClick={onOpenMarqueeMenu} size="small">
-          <ArrowDropDownIcon />
-        </Button>
-      </ButtonGroup>
-
-      <span>&nbsp;&nbsp;</span>
-
-      <ButtonGroup color="inherit" ref={lassoMenuAnchorEl} variant="contained">
-        <Button>
-          <SvgIcon fontSize="small">
-            <LassoMethodIcon />
-          </SvgIcon>
-        </Button>
-
-        <Button color="inherit" onClick={onOpenLassoMenu} size="small">
-          <ArrowDropDownIcon />
-        </Button>
-      </ButtonGroup>
-
-      <span>&nbsp;&nbsp;</span>
-
-      <ButtonGroup
-        color="inherit"
-        ref={probabilisticMenuAnchorEl}
-        variant="contained"
+      <ToggleButton
+        aria-label="elliptical selection"
+        value={SelectionMethod.Elliptical}
       >
-        <Button>
-          <SvgIcon fontSize="small">
-            <ProbabilisticMethodIcon />
-          </SvgIcon>
-        </Button>
+        <SvgIcon fontSize="small">
+          <EllipticalIcon />
+        </SvgIcon>
+      </ToggleButton>
 
-        <Button color="inherit" onClick={onOpenProbabilisticMenu} size="small">
-          <ArrowDropDownIcon />
-        </Button>
-      </ButtonGroup>
+      <ToggleButton aria-label="lasso selection" value={SelectionMethod.Lasso}>
+        <SvgIcon fontSize="small">
+          <LassoIcon />
+        </SvgIcon>
+      </ToggleButton>
 
-      <ButtonGroupMenu
-        anchorEl={marqueeMenuAnchorEl}
-        onClose={onCloseMarqueeMenu}
-        open={openMarqueeMenu}
+      <ToggleButton
+        aria-label="magnetic selection"
+        value={SelectionMethod.Magnetic}
       >
-        <ButtonGroupMenuItem
-          icon={<RectangularIcon />}
-          method={SelectionMethod.Rectangular}
-          name="Rectangular selection"
-          onClick={onClickMarqueeMenuItem}
-        />
+        <SvgIcon fontSize="small">
+          <MagneticIcon />
+        </SvgIcon>
+      </ToggleButton>
 
-        <ButtonGroupMenuItem
-          icon={<EllipticalIcon />}
-          method={SelectionMethod.Elliptical}
-          name="Elliptical selection"
-          onClick={onClickMarqueeMenuItem}
-        />
-      </ButtonGroupMenu>
+      <ToggleButton aria-label="color selection" value={SelectionMethod.Color}>
+        <SvgIcon fontSize="small">
+          <MagicWandIcon />
+        </SvgIcon>
+      </ToggleButton>
 
-      <ButtonGroupMenu
-        anchorEl={lassoMenuAnchorEl}
-        onClose={onCloseLassoMenu}
-        open={openLassoMenu}
-      >
-        <ButtonGroupMenuItem
-          icon={<LassoIcon />}
-          method={SelectionMethod.Lasso}
-          name="Lasso selection"
-          onClick={onClickLassoMenuItem}
-        />
-
-        <ButtonGroupMenuItem
-          icon={<MagneticIcon />}
-          method={SelectionMethod.Magnetic}
-          name="Magnetic selection"
-          onClick={onClickLassoMenuItem}
-        />
-      </ButtonGroupMenu>
-
-      <ButtonGroupMenu
-        anchorEl={probabilisticMenuAnchorEl}
-        onClose={onCloseProbabilisticMenu}
-        open={openProbabilisticMenu}
-      >
-        <ButtonGroupMenuItem
-          icon={<MagicWandIcon />}
-          method={SelectionMethod.Color}
-          name="Color selection"
-          onClick={onClickProbabilisticMenuItem}
-        />
-
-        <ButtonGroupMenuItem
-          icon={<QuickIcon />}
-          method={SelectionMethod.Quick}
-          name="Quick selection"
-          onClick={onClickProbabilisticMenuItem}
-        />
-      </ButtonGroupMenu>
-    </React.Fragment>
+      <ToggleButton aria-label="quick selection" value={SelectionMethod.Quick}>
+        <SvgIcon fontSize="small">
+          <QuickIcon />
+        </SvgIcon>
+      </ToggleButton>
+    </ToggleButtonGroup>
   );
 };
