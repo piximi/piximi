448e1030fc61fdbddd55a443f9e84599e42450ed
alucas <alicelucas93@gmail.com>
2021-01-05 09:45:55 -0500

    konva rectangular select

--- src/components/ImageViewer/RectangularSelection/RectangularSelection.tsx
+++ src/components/ImageViewer/RectangularSelection/RectangularSelection.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import { Image } from "../../../types/Image";
-import { useStyles } from "./RectangularSelection.css";
+import * as ReactKonva from "react-konva";
+import useImage from "use-image";
+import { Stage } from "konva/types/Stage";
 
 type ImageViewerProps = {
   data: Image;
 };
 
 export const RectangularSelection = ({ data }: ImageViewerProps) => {
-  const classes = useStyles();
+  const [img] = useImage(data.src);
 
-  return <div />;
+  const stage = React.useRef<Stage>(null);
+
+  const onMouseDown = () => {};
+
+  const onMouseMove = () => {};
+
+  const onMouseUp = () => {};
+
+  return (
+    <ReactKonva.Stage
+      globalCompositeOperation="destination-over"
+      height={data.shape?.r}
+      onMouseDown={onMouseDown}
+      onMouseMove={onMouseMove}
+      onMouseUp={onMouseUp}
+      ref={stage}
+      width={data.shape?.c}
+    >
+      <ReactKonva.Layer>
+        <ReactKonva.Image image={img} />
+      </ReactKonva.Layer>
+    </ReactKonva.Stage>
+  );
 };
