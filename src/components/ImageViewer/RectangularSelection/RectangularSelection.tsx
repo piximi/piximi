a664928efa9970eb6ea042025ae8444689523d22
pearlryder <pryder@broadinstitute.org>
2021-01-06 08:44:02 -0500

    steadily paced marching ants in rectangular selection

--- src/components/ImageViewer/RectangularSelection/RectangularSelection.tsx
+++ src/components/ImageViewer/RectangularSelection/RectangularSelection.tsx
@@ -29,12 +29,14 @@ export const RectangularSelection = ({ data }: ImageViewerProps) => {
   const [offset, setOffset] = useState<number>(0);
 
   React.useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setOffset(offset + 1);
       if (offset > 32) {
         setOffset(0);
       }
     }, 200);
+
+    return () => clearTimeout(timer);
   });
 
   React.useEffect(() => {
