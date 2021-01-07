d956b3f177eadcffe96e06c9c7152bb06476fa60
Allen Goodman <allen.goodman@icloud.com>
2021-01-07 17:29:24 -0500

    slic

--- src/image/slic/computeSLICSegmentation.ts
+++ src/image/slic/computeSLICSegmentation.ts
@@ -1,6 +1,4 @@
 import { computeResidualError } from "./computeResidualError";
-import { rgb2xyz } from "./rgb2xyz";
-import { xyz2lab } from "./xyz2lab";
 
 export const computeSLICSegmentation = (
   imageData: ImageData,
@@ -21,11 +19,63 @@ export const computeSLICSegmentation = (
   const msMap = new Float32Array(numPixels);
   const distanceMap = new Float32Array(numPixels);
 
-  const labData = xyz2lab(
-    rgb2xyz(imageData.data, imageData.width, imageData.height),
-    imageData.width,
-    imageData.height
-  );
+  /*
+   * RGB to XYZ
+   */
+  const xyz = new Float32Array(3 * imWidth * imHeight);
+
+  const gamma = 2.2;
+
+  for (let i = 0; i < imWidth * imHeight; i++) {
+    const r = Math.pow(
+      parseFloat(String(imageData.data[4 * i])) * 0.00392156862,
+      gamma
+    );
+    const g = Math.pow(
+      parseFloat(String(imageData.data[4 * i + 1])) * 0.00392156862,
+      gamma
+    );
+    const b = Math.pow(
+      parseFloat(String(imageData.data[4 * i + 2])) * 0.00392156862,
+      gamma
+    );
+
+    xyz[i] = r * 0.488718 + g * 0.31068 + b * 0.200602;
+    xyz[i + imWidth * imHeight] = r * 0.176204 + g * 0.812985 + b * 0.0108109;
+    xyz[i + 2 * imWidth * imHeight] = g * 0.0102048 + b * 0.989795;
+  }
+
+  /*
+   * XYZ to Lab
+   */
+  const xw = 1.0 / 3.0;
+  const yw = 1.0 / 3.0;
+  const Yw = 1.0;
+  const Xw = xw / yw;
+  const Zw = ((1 - xw - yw) / yw) * Yw;
+  const ix = 1.0 / Xw;
+  const iy = 1.0 / Yw;
+  const iz = 1.0 / Zw;
+  const lab = new Float32Array(3 * imWidth * imHeight);
+
+  for (let i = 0; i < imWidth * imHeight; i++) {
+    const fx =
+      xyz[i] * ix > 0.00856
+        ? Math.pow(xyz[i] * ix, 0.33333333)
+        : 7.78706891568 * xyz[i] * ix + 0.1379310336;
+    const fy =
+      xyz[imWidth * imHeight + i] * iy > 0.00856
+        ? Math.pow(xyz[imWidth * imHeight + i] * iy, 0.33333333)
+        : 7.78706891568 * xyz[imWidth * imHeight + i] * iy + 0.1379310336;
+    const fz =
+      xyz[2 * imWidth * imHeight + i] * iz > 0.00856
+        ? Math.pow(xyz[2 * imWidth * imHeight + i] * iz, 0.33333333)
+        : 7.78706891568 * xyz[2 * imWidth * imHeight + i] * iz + 0.1379310336;
+
+    lab[i] = 116.0 * fy - 16.0;
+    lab[i + imWidth * imHeight] = 500.0 * (fx - fy);
+    lab[i + 2 * imWidth * imHeight] = 200.0 * (fy - fz);
+  }
 
   /*
    * Compute gradient
@@ -33,33 +83,15 @@ export const computeSLICSegmentation = (
   const gradient = new Float32Array(numPixels);
 
   for (let k = 0; k < 3; k++) {
-    for (let y = 1; y < imageData.height - 1; y++) {
-      for (let x = 1; x < imageData.width - 1; x++) {
-        const a =
-          labData[
-            k * imageData.width * imageData.height + y * imageData.width + x - 1
-          ];
-        const b =
-          labData[
-            k * imageData.width * imageData.height + y * imageData.width + x + 1
-          ];
-        const c =
-          labData[
-            k * imageData.width * imageData.height +
-              (y + 1) * imageData.width +
-              x
-          ];
-        const d =
-          labData[
-            k * imageData.width * imageData.height +
-              (y - 1) * imageData.width +
-              x
-          ];
+    for (let y = 1; y < imHeight - 1; y++) {
+      for (let x = 1; x < imWidth - 1; x++) {
+        const a = lab[k * imWidth * imHeight + y * imWidth + x - 1];
+        const b = lab[k * imWidth * imHeight + y * imWidth + x + 1];
+        const c = lab[k * imWidth * imHeight + (y + 1) * imWidth + x];
+        const d = lab[k * imWidth * imHeight + (y - 1) * imWidth + x];
 
-        gradient[y * imageData.width + x] =
-          gradient[y * imageData.width + x] +
-          (a - b) * (a - b) +
-          (c - d) * (c - d);
+        gradient[y * imWidth + x] =
+          gradient[y * imWidth + x] + (a - b) * (a - b) + (c - d) * (c - d);
       }
     }
   }
@@ -79,20 +111,20 @@ export const computeSLICSegmentation = (
       let x = parseInt(String(Math.round(regionSize * (u + 0.5))), 10);
       let y = parseInt(String(Math.round(regionSize * (v + 0.5))), 10);
 
-      x = Math.max(Math.min(x, imageData.width - 1), 0);
-      y = Math.max(Math.min(y, imageData.height - 1), 0);
+      x = Math.max(Math.min(x, imWidth - 1), 0);
+      y = Math.max(Math.min(y, imHeight - 1), 0);
 
       for (
         let yp = Math.max(0, y - 1);
-        yp <= Math.min(imageData.height - 1, y + 1);
+        yp <= Math.min(imHeight - 1, y + 1);
         yp++
       ) {
         for (
           let xp = Math.max(0, x - 1);
-          xp <= Math.min(imageData.width - 1, x + 1);
+          xp <= Math.min(imWidth - 1, x + 1);
           xp++
         ) {
-          const thisEdgeValue = gradient[yp * imageData.width + xp];
+          const thisEdgeValue = gradient[yp * imWidth + xp];
 
           if (thisEdgeValue < minEdgeValue) {
             minEdgeValue = thisEdgeValue;
@@ -105,19 +137,11 @@ export const computeSLICSegmentation = (
       currentCenters[i++] = parseFloat(String(centerx));
       currentCenters[i++] = parseFloat(String(centery));
 
-      currentCenters[i++] = labData[centery * imageData.width + centerx];
+      currentCenters[i++] = lab[centery * imWidth + centerx];
       currentCenters[i++] =
-        labData[
-          imageData.width * imageData.height +
-            centery * imageData.width +
-            centerx
-        ];
+        lab[imWidth * imHeight + centery * imWidth + centerx];
       currentCenters[i++] =
-        labData[
-          2 * imageData.width * imageData.height +
-            centery * imageData.width +
-            centerx
-        ];
+        lab[2 * imWidth * imHeight + centery * imWidth + centerx];
 
       clusterParams[j++] = 10 * 10;
       clusterParams[j++] = regionSize * regionSize;
@@ -142,25 +166,22 @@ export const computeSLICSegmentation = (
 
       for (
         let y2 = Math.max(0, cy - regionSize);
-        y2 < Math.min(imageData.height, cy + regionSize);
+        y2 < Math.min(imHeight, cy + regionSize);
         ++y2
       ) {
         for (
           let x2 = Math.max(0, cx - regionSize);
-          x2 < Math.min(imageData.width, cx + regionSize);
+          x2 < Math.min(imWidth, cx + regionSize);
           ++x2
         ) {
           const spatial = (x2 - cx) * (x2 - cx) + (y2 - cy) * (y2 - cy);
-          const dR =
-            labData[y2 * imageData.width + x2] - currentCenters[5 * region + 2];
+          const dR = lab[y2 * imWidth + x2] - currentCenters[5 * region + 2];
           const dG =
-            labData[
-              imageData.width * imageData.height + y2 * imageData.width + x2
-            ] - currentCenters[5 * region + 3];
+            lab[imWidth * imHeight + y2 * imWidth + x2] -
+            currentCenters[5 * region + 3];
           const dB =
-            labData[
-              2 * imageData.width * imageData.height + y2 * imageData.width + x2
-            ] - currentCenters[5 * region + 4];
+            lab[2 * imWidth * imHeight + y2 * imWidth + x2] -
+            currentCenters[5 * region + 4];
           const appearance = dR * dR + dG * dG + dB * dB;
 
           const distance = Math.sqrt(
@@ -168,30 +189,30 @@ export const computeSLICSegmentation = (
               spatial / clusterParams[region * 2 + 1]
           );
 
-          if (distance < distanceMap[y2 * imageData.width + x2]) {
-            distanceMap[y2 * imageData.width + x2] = distance;
-            segmentation[y2 * imageData.width + x2] = region;
+          if (distance < distanceMap[y2 * imWidth + x2]) {
+            distanceMap[y2 * imWidth + x2] = distance;
+            segmentation[y2 * imWidth + x2] = region;
           }
         }
       }
     }
 
-    for (let y2 = 0; y2 < imageData.height; ++y2) {
-      for (let x2 = 0; x2 < imageData.width; ++x2) {
+    for (let y2 = 0; y2 < imHeight; ++y2) {
+      for (let x2 = 0; x2 < imWidth; ++x2) {
         if (
-          clusterParams[segmentation[y2 * imageData.width + x2] * 2] <
-          mcMap[y2 * imageData.width + x2]
+          clusterParams[segmentation[y2 * imWidth + x2] * 2] <
+          mcMap[y2 * imWidth + x2]
         ) {
-          clusterParams[segmentation[y2 * imageData.width + x2] * 2] =
-            mcMap[y2 * imageData.width + x2];
+          clusterParams[segmentation[y2 * imWidth + x2] * 2] =
+            mcMap[y2 * imWidth + x2];
         }
 
         if (
-          clusterParams[segmentation[y2 * imageData.width + x2] * 2 + 1] <
-          msMap[y2 * imageData.width + x2]
+          clusterParams[segmentation[y2 * imWidth + x2] * 2 + 1] <
+          msMap[y2 * imWidth + x2]
         ) {
-          clusterParams[segmentation[y2 * imageData.width + x2] * 2 + 1] =
-            msMap[y2 * imageData.width + x2];
+          clusterParams[segmentation[y2 * imWidth + x2] * 2 + 1] =
+            msMap[y2 * imWidth + x2];
         }
       }
     }
@@ -231,23 +252,19 @@ export const computeSLICSegmentation = (
      */
     let region;
 
-    for (let y1 = 0; y1 < imageData.height; y1++) {
-      for (let x1 = 0; x1 < imageData.width; x1++) {
-        region = segmentation[x1 + y1 * imageData.width];
+    for (let y1 = 0; y1 < imHeight; y1++) {
+      for (let x1 = 0; x1 < imWidth; x1++) {
+        region = segmentation[x1 + y1 * imWidth];
 
         masses[region]++;
 
         newCenters[region * 5] += x1;
         newCenters[region * 5 + 1] += y1;
-        newCenters[region * 5 + 2] += labData[y1 * imageData.width + x1];
+        newCenters[region * 5 + 2] += lab[y1 * imWidth + x1];
         newCenters[region * 5 + 3] +=
-          labData[
-            imageData.width * imageData.height + y1 * imageData.width + x1
-          ];
+          lab[imWidth * imHeight + y1 * imWidth + x1];
         newCenters[region * 5 + 4] +=
-          labData[
-            2 * imageData.width * imageData.height + y1 * imageData.width + x1
-          ];
+          lab[2 * imWidth * imHeight + y1 * imWidth + x1];
       }
     }
 
@@ -301,19 +318,19 @@ export const computeSLICSegmentation = (
 
     cleanedLabel = label + 1;
     cleaned[pixel] = label + 1;
-    x1 = pixel % imageData.width;
-    y1 = Math.floor(pixel / imageData.width);
+    x1 = pixel % imWidth;
+    y1 = Math.floor(pixel / imWidth);
 
     for (let direction = 0; direction < 4; direction++) {
       xp = x1 + dx[direction];
       yp = y1 + dy[direction];
-      neighbor = xp + yp * imageData.width;
+      neighbor = xp + yp * imWidth;
 
       if (
         0 <= xp &&
-        xp < imageData.width &&
+        xp < imWidth &&
         0 <= yp &&
-        yp < imageData.height &&
+        yp < imHeight &&
         cleaned[neighbor]
       ) {
         cleanedLabel = cleaned[neighbor];
@@ -323,19 +340,19 @@ export const computeSLICSegmentation = (
     while (numExpanded < segmentSize) {
       const open = segment[numExpanded++];
 
-      x1 = open % imageData.width;
-      y1 = Math.floor(open / imageData.width);
+      x1 = open % imWidth;
+      y1 = Math.floor(open / imWidth);
 
       for (let direction = 0; direction < 4; ++direction) {
         xp = x1 + dx[direction];
         yp = y1 + dy[direction];
-        neighbor = xp + yp * imageData.width;
+        neighbor = xp + yp * imWidth;
 
         if (
           0 <= xp &&
-          xp < imageData.width &&
+          xp < imWidth &&
           0 <= yp &&
-          yp < imageData.height &&
+          yp < imHeight &&
           cleaned[neighbor] === 0 &&
           segmentation[neighbor] === label
         ) {
