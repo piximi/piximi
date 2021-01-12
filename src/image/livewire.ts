f2027c022fa07d3c4c81e49e4ce72962ca4965ac
alucas <alicelucas93@gmail.com>
2021-01-12 09:38:55 -0500

    livewire

--- src/image/livewire.ts
+++ src/image/livewire.ts
@@ -7,10 +7,8 @@ const cost = (image: [], width: number, height: number) => {
     for (x = 0; x < height; x++) {
       const idx = (width * y + x) * 4;
       grayscale[y][x] = (image[idx] + image[idx + 1] + image[idx + 2]) / 3;
-      grayscale[y][x] = grayscale[y][x];
     }
   }
-
   //OPTIONAL: compute edges
 };
 
@@ -21,39 +19,68 @@ type metadata = {
   y: number;
   e: boolean;
   g: number;
-  p: { x: number | null; y: number | null };
-}[][];
+  p: { x: number; y: number } | null;
+};
 
 export const livewire = (
   seed: { x: number; y: number },
-  data: metadata,
-  cost: number[][]
+  metadata: metadata[][],
+  cost: number[][],
+  width: number,
+  height: number
 ) => {
+  //initialize metadata
+  let data: metadata[][] = [];
+  for (let y = 0; y < height; y++) {
+    data[y] = [];
+    for (let x = 0; x < width; x++) {
+      data[y][x] = { x: x, y: y, e: false, g: infinity, p: null };
+    }
+  }
+
   const L: { x: number; y: number }[] = []; // list of active pixels, sorted by total cost
   let q: { x: number; y: number } | undefined;
   let gtmp: number;
+  let r: metadata;
 
-  L.push({ x: seed.x, y: seed.y }); // this should be a priority queue
-  data[seed.y][seed.x].g = 0;
+  L.push({ x: seed.x, y: seed.y });
+  metadata[seed.y][seed.x].g = 0;
 
   while (L.length !== 0) {
     q = L.pop(); // get pixel coordinate with minimum total cost
     if (q && q.x && q.y) {
-      data[q.y][q.x].e = true; // label pixel as expanded
+      metadata[q.y][q.x].e = true; // label pixel as expanded
       //for each neighhbor pixel
       let neighbours = [
-        data[q.y - 1][q.x - 1],
-        data[q.y - 1][q.x],
-        data[q.y - 1][q.x + 1],
-        data[q.y][q.x - 1],
-        data[q.y][q.x + 2],
-        data[q.y + 1][q.x - 1],
-        data[q.y + 1][q.x],
-        data[q.y + 1][q.x + 1],
+        metadata[q.y - 1][q.x - 1],
+        metadata[q.y - 1][q.x],
+        metadata[q.y - 1][q.x + 1],
+        metadata[q.y][q.x - 1],
+        metadata[q.y][q.x + 2],
+        metadata[q.y + 1][q.x - 1],
+        metadata[q.y + 1][q.x],
+        metadata[q.y + 1][q.x + 1],
       ];
-
-      //[implement] for each neighbor that has NNOT been visited...
-      gtmp = data[q.y][q.x].g + cost[q.y][q.x];
+      neighbours = neighbours.filter(
+        (neighbour) => !metadata[neighbour.y][neighbour.x].e
+      );
+      for (let idx = 0; idx < neighbours.length; idx++) {
+        r = neighbours[idx];
+        gtmp = r.g + cost[r.y][r.x];
+        let rindex = L.indexOf({ x: r.x, y: r.y });
+        if (rindex > -1 && gtmp < r.g) {
+          L.splice(rindex, 1);
+        }
+        if (rindex === -1) {
+          r.g = gtmp;
+          r.p = { x: q.x, y: q.y };
+          L.push({ x: r.x, y: r.y });
+          // sort list in descending order, so that a pop() operation gives the pixel with smallest total cost
+          L.sort(function (a, b) {
+            return metadata[b.y][b.x].g - metadata[a.y][a.x].g;
+          });
+        }
+      }
     }
   }
 };
