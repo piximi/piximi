6488ce3cb552f0a2032820bb92075c1c461973ec
Allen Goodman <allen.goodman@icloud.com>
2021-01-19 10:23:09 -0500

    EllipticalSelection

--- src/image/Selection/EllipticalSelection.ts
+++ src/image/Selection/EllipticalSelection.ts
@@ -1,9 +1,27 @@
 import { Selection } from "./Selection";
 
 export class EllipticalSelection extends Selection {
-  public onMouseDown(position: { x: number; y: number }): void {}
+  public center?: { x: number; y: number };
+  public origin?: { x: number; y: number };
+  public radius?: { x: number; y: number };
 
-  public onMouseMove(position: { x: number; y: number }): void {}
+  public onMouseDown(position: { x: number; y: number }): void {
+    this.origin = position;
+  }
+
+  public onMouseMove(position: { x: number; y: number }): void {
+    if (this.origin) {
+      this.center = {
+        x: (position.x - this.origin.x) / 2 + this.origin.x,
+        y: (position.y - this.origin.y) / 2 + this.origin.y,
+      };
+
+      this.radius = {
+        x: Math.abs((position.x - this.origin.x) / 2),
+        y: Math.abs((position.y - this.origin.y) / 2),
+      };
+    }
+  }
 
   public onMouseUp(position: { x: number; y: number }): void {}
 }
