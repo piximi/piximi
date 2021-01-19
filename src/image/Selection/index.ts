e6bdb37a26c5b7237bed4756ea7a3dc3eefb5b10
Allen Goodman <allen.goodman@icloud.com>
2021-01-19 09:36:31 -0500

    exports

--- src/image/Selection/index.ts
+++ src/image/Selection/index.ts
@@ -1,1 +1,8 @@
 export { ColorSelection } from "./ColorSelection";
+export { EllipticalSelection } from "./EllipticalSelection";
+export { LassoSelection } from "./LassoSelection";
+export { MagneticSelection } from "./MagneticSelection";
+export { ObjectSelection } from "./ObjectSelection";
+export { PolygonalSelection } from "./PolygonalSelection";
+export { QuickSelection } from "./QuickSelection";
+export { RectangularSelection } from "./RectangularSelection";
