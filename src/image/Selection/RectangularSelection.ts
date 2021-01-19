05fe12cfde110bd81dd8debcc53aa9ecebc6dd47
Allen Goodman <allen.goodman@icloud.com>
2021-01-19 10:25:07 -0500

    EllipticalSelection

--- src/image/Selection/RectangularSelection.ts
+++ src/image/Selection/RectangularSelection.ts
@@ -3,25 +3,19 @@ import { Selection } from "./Selection";
 export class RectangularSelection extends Selection {
   public c?: number;
   public r?: number;
-  public x?: number;
-  public y?: number;
+
+  public origin?: { x: number; y: number };
 
   public onMouseDown(position: { x: number; y: number }): void {
-    this.x = position.x;
-    this.y = position.y;
+    this.origin = position;
   }
 
   public onMouseMove(position: { x: number; y: number }): void {
-    if (this.x && this.y) {
-      this.c = position.x - this.x;
-      this.r = position.y - this.y;
+    if (this.origin) {
+      this.c = position.x - this.origin.x;
+      this.r = position.y - this.origin.y;
     }
   }
 
-  public onMouseUp(position: { x: number; y: number }): void {
-    if (this.x && this.y) {
-      this.c = position.x - this.x;
-      this.r = position.y - this.y;
-    }
-  }
+  public onMouseUp(position: { x: number; y: number }): void {}
 }
