428d2b1a3d7e1379711268e37cb6ee05f726641f
Allen Goodman <allen.goodman@icloud.com>
2021-01-20 13:30:06 -0500

    open image

--- src/components/ImageViewer/OpenImageButton/OpenImageButton.tsx
+++ src/components/ImageViewer/OpenImageButton/OpenImageButton.tsx
@@ -3,27 +3,66 @@ import { useStyles } from "./OpenImageButton.css";
 import { Button } from "@material-ui/core";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import Tooltip from "@material-ui/core/Tooltip";
-import { useMenu } from "../../../hooks";
-import { UploadMenu } from "../../UploadMenu";
+import { Shape } from "../../../types/Shape";
+import { createImage } from "../../../store/slices";
+import { useDispatch } from "react-redux";
 
 export const OpenImageButton = () => {
-  const { anchorEl, onClose, onOpen, open } = useMenu();
+  const dispatch = useDispatch();
 
   const classes = useStyles();
 
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+
+    if (event.currentTarget.files) {
+      const blob = event.currentTarget.files[0];
+
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        if (event.target) {
+          const src = event.target.result;
+
+          const image = new Image();
+
+          image.onload = () => {
+            const shape: Shape = {
+              r: image.naturalHeight,
+              c: image.naturalWidth,
+              channels: 4,
+            };
+
+            dispatch(createImage({ shape: shape, src: src as string }));
+          };
+
+          image.src = src as string;
+        }
+      };
+
+      reader.readAsDataURL(blob);
+    }
+  };
+
   return (
     <React.Fragment>
-      <Tooltip title="Open image">
-        <Button
-          className={classes.button}
-          onClick={onOpen}
-          startIcon={<CloudUploadIcon />}
-        >
-          Open image
-        </Button>
-      </Tooltip>
+      <div>
+        <input
+          accept="image/*"
+          hidden
+          type="file"
+          id="open-image"
+          onChange={onChange}
+        />
 
-      <UploadMenu anchorEl={anchorEl!.current} onClose={onClose} open={open} />
+        <Tooltip title="Open image">
+          <label htmlFor="open-image">
+            <Button className={classes.button} startIcon={<CloudUploadIcon />}>
+              Open image
+            </Button>
+          </label>
+        </Tooltip>
+      </div>
     </React.Fragment>
   );
 };
