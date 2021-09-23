79f01b12597d0f14bcbdba6d8e9c1f48dcc4314b
alucas <alicelucas93@gmail.com>
2021-09-23 16:23:29 -0400

    PredictSegmenterDialog

--- src/components/PredictSegmenterDialog/PredictSegmenterDialog/PredictSegmenterDialog.tsx
+++ src/components/PredictSegmenterDialog/PredictSegmenterDialog/PredictSegmenterDialog.tsx
@@ -4,25 +4,68 @@ import * as tf from "@tensorflow/tfjs";
 import { PredictSegmenterDialogAppBar } from "../PredictSegmenterDialogAppBar";
 import { DialogTransition } from "../DialogTransition";
 import { useStyles } from "./PredictSegmenterDialog.css";
-import { Image } from "../../../types/Image";
 import Container from "@material-ui/core/Container";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import { ImageGridAppBar } from "../../ImageGridAppBar";
 import { useSelector } from "react-redux";
 import { imagesSelector } from "../../../store/selectors";
-import { tileSizeSelector } from "../../../store/selectors/tileSizeSelector";
 import * as ImageJS from "image-js";
+
+const overlayLabels = (imageData: Uint8Array, labelData: Uint8Array) => {
+  const labelOpacity = 0.3;
+  const imgOpacity = 1 - labelOpacity;
+
+  const colorData = [];
+  const [r, g, b] = [255, 0, 0]; // color of label
+  for (let i = 0; i < labelData.length; i += 4) {
+    if (labelData[i] === 0) {
+      colorData.push(imageData[i] * imgOpacity);
+      colorData.push(imageData[i + 1] * imgOpacity);
+      colorData.push(imageData[i + 2] * imgOpacity);
+      colorData.push(255);
+    } else {
+      colorData.push(imageData[i] * imgOpacity + r * labelOpacity);
+      colorData.push(imageData[i + 1] * imgOpacity + g * labelOpacity);
+      colorData.push(imageData[i + 2] * imgOpacity + b * labelOpacity);
+      colorData.push(255);
+    }
+  }
+
+  const [width, height] = [216, 216]; //FIXME this should not be hard coded
+
+  //look at image
+  const overlay = new ImageJS.Image(width, height, colorData, {
+    alpha: 1,
+    components: 3,
+  });
+
+  return overlay;
+};
+
+const forwardPass = async (image: ImageJS.Image, imgSize: number, net: any) => {
+  console.info("Predicting...");
+
+  const out = tf.tidy(() => {
+    // tf.browser.fromPixels() returns a Tensor from an image element.
+    const img = tf.cast(tf.browser.fromPixels(image.getCanvas()), "float32");
+
+    const offset = tf.scalar(127.5);
+    // Normalize the image from [0, 255] to [-1, 1].
+    const normalized = img.sub(offset).div(offset);
+
+    // Reshape to a single-element batch so we can pass it to predict.
+    const batched = normalized.reshape([1, imgSize, imgSize, 3]);
+
+    //make prediction
+    return net.predict(batched);
+  });
+
+  const probs = tf.squeeze(tf.argMax(out, 3)) as tf.Tensor2D;
+  return await tf.browser.toPixels(probs);
+};
 
 type EvaluateClassifierDialogProps = {
   closeDialog: () => void;
   openedDialog: boolean;
 };
-
-function roundToFour(num: number) {
-  // @ts-ignore
-  return +(Math.round(num + "e+4") + "e-4");
-}
 
 export const PredictSegmenterDialog = (
   props: EvaluateClassifierDialogProps
@@ -33,30 +76,7 @@ export const PredictSegmenterDialog = (
 
   const images = useSelector(imagesSelector);
 
-  const forwardPass = async (image: Image, imgSize: number, net: any) => {
-    console.info("Predicting...");
-
-    const data = await ImageJS.Image.load(image.src);
-
-    const out = tf.tidy(() => {
-      // tf.browser.fromPixels() returns a Tensor from an image element.
-      const img = tf.cast(tf.browser.fromPixels(data.getCanvas()), "float32");
-
-      const offset = tf.scalar(127.5);
-      // Normalize the image from [0, 255] to [-1, 1].
-      const normalized = img.sub(offset).div(offset);
-
-      // Reshape to a single-element batch so we can pass it to predict.
-      const resized = normalized.resizeBilinear([imgSize, imgSize]);
-      const batched = resized.reshape([1, imgSize, imgSize, 3]);
-
-      //make prediction
-      return net.predict(batched);
-    });
-
-    const probs = tf.squeeze(tf.argMax(out, 3)) as tf.Tensor2D;
-    return await tf.browser.toPixels(probs);
-  };
+  const [imageData, setImageData] = React.useState<string>(images[0].src);
 
   const predictTensorflowModel = async () => {
     const image = images[0]; //FIXME: in the future, ask the user to select an image
@@ -66,6 +86,7 @@ export const PredictSegmenterDialog = (
       "https://storage.piximi.app/examples/unet/model.json";
 
     const IMAGE_SIZE = 224;
+    const BORDER = 4; //account for border effects
 
     const model = await tf.loadLayersModel(MOBILENET_MODEL_PATH);
     // Warmup the model. This isn't necessary, but makes the first prediction
@@ -79,50 +100,30 @@ export const PredictSegmenterDialog = (
 
     if (!image) return;
 
-    const labels = await forwardPass(image, IMAGE_SIZE, model);
+    //Resize image to [IMAGE_SIZE, IMAGE_SIZE]
+    const img = await ImageJS.Image.load(image.src);
+    const resizedImg = img.resize({ width: IMAGE_SIZE, height: IMAGE_SIZE });
 
-    const colorData = [];
-    const [r, g, b] = [255, 0, 0];
-    for (let i = 0; i < labels.length; i += 4) {
-      if (labels[i] === 0) {
-        colorData.push(0);
-        colorData.push(0);
-        colorData.push(0);
-        colorData.push(255);
-      } else {
-        colorData.push(r);
-        colorData.push(g);
-        colorData.push(b);
-        colorData.push(255);
-      }
-    }
+    const labelData = await forwardPass(resizedImg, IMAGE_SIZE, model);
 
-    const [width, height] = [216, 216]; //FIXME this should not be hard coded
-
-    //look at image
-    const result = new ImageJS.Image(width, height, colorData, {
-      alpha: 1,
-      components: 3,
+    const cropped = resizedImg.crop({
+      x: BORDER,
+      y: BORDER,
+      height: IMAGE_SIZE - 2 * BORDER,
+      width: IMAGE_SIZE - 2 * BORDER,
     });
-    console.info(result.toDataURL());
+
+    const overlay = overlayLabels(
+      Uint8Array.from(cropped.data) as Uint8Array,
+      Uint8Array.from(labelData)
+    );
+
+    setImageData(overlay.toDataURL());
   };
 
   const onPredict = async () => {
     await predictTensorflowModel().then(() => {});
   };
-
-  const getSize = (scaleFactor: number) => {
-    const width = (230 * scaleFactor).toString() + "px";
-    const height = (185 * scaleFactor).toString() + "px";
-    return {
-      width: width,
-      height: height,
-      background: "lightgray",
-      margin: "2px",
-    };
-  };
-
-  const scaleFactor = useSelector(tileSizeSelector);
 
   return (
     // @ts-ignore
@@ -140,34 +141,9 @@ export const PredictSegmenterDialog = (
         evaluate={onPredict}
       />
       <div>
-        <Container className={classes.container} maxWidth={false}>
-          <GridList
-            className={classes.gridList}
-            cols={Math.floor(4 / scaleFactor)}
-            cellHeight="auto"
-          >
-            {images.map((image: Image) => (
-              <GridListTile key={image.id} style={getSize(scaleFactor)}>
-                <img alt="" src={image.src} className={classes.imageTile} />
-              </GridListTile>
-            ))}
-          </GridList>
-
-          <ImageGridAppBar />
+        <Container className={classes.container} maxWidth={"sm"}>
+          <img alt="" src={imageData} className={classes.imageTile} />
         </Container>
-        {/*<Grid container spacing={3}>*/}
-        {/*  <Grid id="evaluationID">*/}
-        {/*    <Paper*/}
-        {/*      style={{*/}
-        {/*        margin: "24px",*/}
-        {/*        padding: "24px",*/}
-        {/*        fontSize: "larger",*/}
-        {/*      }}*/}
-        {/*    >*/}
-        {/*      accuracy: {accuracy}*/}
-        {/*    </Paper>*/}
-        {/*  </Grid>*/}
-        {/*</Grid>*/}
       </div>
 
       <DialogContent style={{ padding: "0px", margin: "12px" }}>
