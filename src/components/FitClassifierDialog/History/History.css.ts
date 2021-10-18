b510333abfb054eeca3c0bf663443e1d3c34e91e
alucas <alicelucas93@gmail.com>
2021-10-18 11:18:12 -0400

    migrate to mui v5

--- src/components/FitClassifierDialog/History/History.css.ts
+++ src/components/FitClassifierDialog/History/History.css.ts
@@ -1,5 +1,4 @@
-import { createStyles } from "@material-ui/styles";
-import { Theme } from "@material-ui/core";
+import { createStyles, Theme } from "@material-ui/core/styles";
 
 export const styles = (theme: Theme) =>
   createStyles({
