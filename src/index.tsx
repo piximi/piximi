420c7e493fe53ac908de474dee3d311dbbdcdb22
alucas <alicelucas93@gmail.com>
2021-10-18 16:49:01 -0400

    migrate to v5 - wrap Application in ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,18 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { productionStore } from "./store/stores";
 import { Application } from "./components/Application";
+import { ThemeProvider } from "@mui/styles";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme();
 
 ReactDOM.render(
   <Provider store={productionStore}>
-    <Application />
+    {/*
+ // @ts-ignore */}
+    <ThemeProvider theme={theme}>
+      <Application />
+    </ThemeProvider>
   </Provider>,
   document.getElementById("root")
 );
