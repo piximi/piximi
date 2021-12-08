7e566ee99ddc8f783b32425b9d7a8cfb6d903b4e
Levin Moser <lmoser@broadinstitute.org>
2021-12-08 10:38:43 -0500

    remove unused Sagas

--- src/store/sagas/rootSaga.ts
+++ src/store/sagas/rootSaga.ts
@@ -1,18 +1,9 @@
 import { all, fork } from "redux-saga/effects";
-import { watchCompileSaga } from "./classifier";
 import { watchFitSaga } from "./classifier";
-import { watchPreprocessSaga } from "./classifier";
-import { watchOpenSaga } from "./classifier";
 import { watchPredictSaga } from "./classifier/watchPredictSaga";
 
 export function* rootSaga() {
-  const effects = [
-    fork(watchCompileSaga),
-    fork(watchFitSaga),
-    fork(watchPredictSaga),
-    fork(watchPreprocessSaga),
-    fork(watchOpenSaga),
-  ];
+  const effects = [fork(watchFitSaga), fork(watchPredictSaga)];
 
   yield all(effects);
 }
