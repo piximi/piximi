35a69a5a3a5865f10779fa8457210d1572c79597
Levin Moser <lmoser@broadinstitute.org>
2021-12-14 17:55:11 -0500

    get image shape from store for predict preprocessing

--- src/store/sagas/classifier/predictSaga.ts
+++ src/store/sagas/classifier/predictSaga.ts
@@ -1,30 +1,34 @@
 import { put, select } from "redux-saga/effects";
 import { projectSlice } from "../../slices";
-import { createdCategoriesSelector, openedSelector } from "../../selectors";
+import { createdCategoriesSelector } from "../../selectors";
 import { rescaleOptionsSelector } from "../../selectors/rescaleOptionsSelector";
 import { preprocess_predict } from "../../coroutines/classifier/preprocess_predict";
 import { predictCategories } from "../../coroutines/classifier/predictCategories";
 import { testImagesSelector } from "../../selectors/testImagesSelector";
 import { fittedSelector } from "../../selectors/fittedSelector";
+import { architectureOptionsSelector } from "../../selectors/architectureOptionsSelector";
 
 export function* predictSaga(action: any): any {
   const rescaleOptions = yield select(rescaleOptionsSelector);
 
   let model = yield select(fittedSelector);
 
-  if (!model) {
-    model = yield select(openedSelector);
-  }
-
-  if (!model) return;
+  const architectureOptions = yield select(architectureOptionsSelector);
 
   const categories = yield select(createdCategoriesSelector);
 
   const testImages = yield select(testImagesSelector);
 
-  if (!testImages.length) return;
+  if (!testImages.length) {
+    alert("No unlabled images to predict!");
+    return;
+  }
 
-  const data = yield preprocess_predict(testImages, rescaleOptions);
+  const data = yield preprocess_predict(
+    testImages,
+    rescaleOptions,
+    architectureOptions.inputShape
+  );
 
   const { imageIds, categoryIds } = yield predictCategories(
     model,
