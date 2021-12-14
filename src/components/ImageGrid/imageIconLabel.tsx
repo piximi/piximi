e82192c5768d44b351fa1996876ab718b7d2ee61
Levin Moser <lmoser@broadinstitute.org>
2021-12-14 17:56:07 -0500

    small image grid refactoring
    - use different icon for predicted images
    - add tooltip to display the category names

---
+++ src/components/ImageGrid/imageIconLabel.tsx
@@ -0,0 +1,67 @@
+import { Tooltip } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import LabelIcon from "@mui/icons-material/Label";
+import { useSelector } from "react-redux";
+import { Category, UNKNOWN_CATEGORY_ID } from "../../types/Category";
+import { Image } from "../../types/Image";
+import { Partition } from "../../types/Partition";
+import { categoriesSelector } from "../../store/selectors/categoriesSelector";
+
+type ImageIconLabelProbs = {
+  image: Image;
+};
+
+export const ImageIconLabel = ({ image }: ImageIconLabelProbs) => {
+  const categories = useSelector(categoriesSelector);
+
+  const color = categories.find((category: Category) => {
+    return category.id === image.categoryId;
+  })?.color as string;
+
+  const categoryName = categories.find((category: Category) => {
+    return category.id === image.categoryId;
+  })?.name as string;
+
+  const actionIconStyle = {
+    color: color,
+    marginLeft: "8px",
+    marginTop: "8px",
+  };
+
+  const useStyles = makeStyles(() => ({
+    iconLabelTooltip: {
+      borderRadius: 2,
+      backgroundColor: color,
+    },
+    customArrow: {
+      color: color,
+    },
+  }));
+
+  const classes = useStyles();
+
+  const predictedLabel =
+    image.partition === Partition.Inference &&
+    image.categoryId !== UNKNOWN_CATEGORY_ID;
+
+  return (
+    <>
+      <Tooltip
+        title={categoryName}
+        placement="right"
+        arrow
+        classes={{
+          tooltip: classes.iconLabelTooltip,
+          arrow: classes.customArrow,
+        }}
+      >
+        {predictedLabel ? (
+          <LabelImportantIcon sx={actionIconStyle} />
+        ) : (
+          <LabelIcon sx={actionIconStyle} />
+        )}
+      </Tooltip>
+    </>
+  );
+};
