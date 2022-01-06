01cb17a8a95382a02a9b3b1233caa424ae619266
Nodar Gogoberidze <gnodar01@gmail.com>
2022-01-06 17:57:30 -0500

    Create routes for Main and Annotator views
    Move all top level components from index to `Application`
    Have `Application` include routes to `MainView` and `AnnotatorView`
    Have `MainView` render primary component tree (previously in `Application`)
    Have `AnnotatorView` render annotator component tree (`ImageViewer`)

--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -1,71 +1,32 @@
-import React, { useCallback } from "react";
-import { ApplicationDrawer } from "../ApplicationDrawer";
-import { ImageGrid } from "../ImageGrid";
-import { ApplicationAppBar } from "../ApplicationAppBar";
-import { Box, CssBaseline } from "@mui/material";
-import { useStyles } from "./Application.css";
-import * as ImageJS from "image-js";
-import { Image } from "../../types/Image";
-import { v4 } from "uuid";
-import { Partition } from "../../types/Partition";
-import { createImage } from "../../store/slices";
-import { useDispatch } from "react-redux";
-import { Shape } from "../../types/Shape";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { ThemeProvider } from "@mui/styles";
+import { StyledEngineProvider } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
+import { productionStore } from "../../store/stores";
+import { MainView } from "../MainView";
+import { AnnotatorView } from "../annotator";
 
 export const Application = () => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  const onDrop = useCallback(
-    (item) => {
-      if (item) {
-        for (let i = 0; i < item.files.length; i++) {
-          const file = item.files[i];
-
-          file.arrayBuffer().then((buffer: any) => {
-            ImageJS.Image.load(buffer).then((image) => {
-              //check whether name already exists
-              const shape: Shape = {
-                channels: image.components,
-                frames: 1,
-                height: image.height,
-                planes: 1,
-                width: image.width,
-              };
-
-              const imageDataURL = image.toDataURL("image/png", {
-                useCanvas: true,
-              });
-
-              const loaded: Image = {
-                categoryId: "00000000-0000-0000-0000-000000000000",
-                id: v4(),
-                annotations: [],
-                name: file.name,
-                partition: Partition.Inference,
-                shape: shape,
-                originalSrc: imageDataURL,
-                src: imageDataURL,
-              };
-
-              dispatch(createImage({ image: loaded }));
-            });
-          });
-        }
-      }
-    },
-    [dispatch]
-  );
+  const theme = createTheme();
 
   return (
-    <Box className={classes.body}>
-      <CssBaseline />
-
-      <ApplicationAppBar />
-
-      <ApplicationDrawer />
-
-      <ImageGrid onDrop={onDrop} />
-    </Box>
+    <Provider store={productionStore}>
+      <ThemeProvider theme={theme}>
+        <StyledEngineProvider injectFirst>
+          <DndProvider backend={HTML5Backend}>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<MainView />} />
+                <Route path="annotator" element={<AnnotatorView />} />
+              </Routes>
+            </BrowserRouter>
+          </DndProvider>
+        </StyledEngineProvider>
+      </ThemeProvider>
+    </Provider>
   );
 };
---
+++ src/components/annotator/AnnotatorView/index.ts
@@ -0,0 +1,1 @@
+export { AnnotatorView } from "./AnnotatorView";
