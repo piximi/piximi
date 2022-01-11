800fd712284f87aa26af779f89660f050afa0fef
Nodar Gogoberidze <gnodar01@gmail.com>
2022-01-11 14:00:12 -0500

    ExampleImageDialog: discard unused css

--- src/components/annotator/CategoriesList/ExampleImageDialog/ExampleImageDialog.css.ts
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from "@mui/styles";
-import { Theme } from "@mui/material";
-
-export const useStyles = makeStyles((theme: Theme) => ({
-  button: {
-    margin: theme.spacing(1),
-    textTransform: "none",
-  },
-}));
