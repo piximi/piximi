effa492200177df29a08586634d88e0e5bf86f28
Nodari Gogoberidze <gnodar01@gmail.com>
2022-01-13 09:27:51 -0500

    ApplicationAppBar: refactor use of makeStyles to use sx prop

--- src/components/ApplicationAppBar/ApplicationAppBar.tsx
+++ src/components/ApplicationAppBar/ApplicationAppBar.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { ApplicationToolbar } from "../ApplicationToolbar";
-import { useStyles } from "./ApplicationAppBar.css";
-import { AppBar } from "@mui/material";
+import { AppBar, Box } from "@mui/material";
 
 export const ApplicationAppBar = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.grow}>
-      <AppBar className={classes.appBar} color="inherit" position="fixed">
+    <Box>
+      <AppBar
+        sx={{
+          borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
+          boxShadow: "none",
+        }}
+        color="inherit"
+        position="fixed"
+      >
         <ApplicationToolbar />
       </AppBar>
-    </div>
+    </Box>
   );
 };
