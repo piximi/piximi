4bfab4d6427b938a744943e74c93ea7c0a2c724d
Nodari Gogoberidze <gnodar01@gmail.com>
2022-01-13 21:34:27 -0500

    ArchitectureSettingsListItem: Refactor use of makeStyles to use of styled component

--- src/components/FitClassifierDialog/ArchitectureSettingsListItem/ArchitectureSettingsListItem.tsx
+++ src/components/FitClassifierDialog/ArchitectureSettingsListItem/ArchitectureSettingsListItem.tsx
@@ -1,74 +1,15 @@
 import * as React from "react";
 import { useState } from "react";
 import { ArchitectureSettingsGrid } from "./ArchitectureSettingsGrid/ArchitectureSettingsGrid";
-import { createStyles, makeStyles } from "@mui/styles";
 import {
   Collapse,
   ListItem,
   ListItemIcon,
   ListItemText,
-  Theme,
+  styled,
 } from "@mui/material";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    expansionPanel: {
-      boxShadow: "none",
-    },
-    leftIcon: {
-      marginRight: theme.spacing(1),
-    },
-    rightIcon: {
-      marginLeft: theme.spacing(1),
-    },
-    button: {
-      marginRight: theme.spacing(1),
-    },
-    grow: {
-      flexGrow: 1,
-    },
-    form: {},
-    appBar: {
-      position: "relative",
-      backgroundColor: "transparent",
-      boxShadow: "none",
-      borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
-    },
-    container: {
-      // width: '100%',
-      display: "flex",
-      flexWrap: "wrap",
-    },
-    root: {
-      zIndex: 1100,
-    },
-    paper: {
-      zIndex: 1100,
-    },
-    paperFullScreen: {
-      left: "280px",
-    },
-    menu: {
-      // width: 200,
-    },
-    textField: {
-      // marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      flexBasis: 300,
-      width: "100%",
-    },
-    title: {
-      marginLeft: theme.spacing(2),
-      flex: 1,
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-  })
-);
 
 export const ArchitectureSettingsListItem = () => {
   const [collapsedClassifierSettingsList, setCollapsedClassifierSettingsList] =
@@ -78,7 +19,11 @@ export const ArchitectureSettingsListItem = () => {
     setCollapsedClassifierSettingsList(!collapsedClassifierSettingsList);
   };
 
-  const classes = useStyles({});
+  const StyledForm = styled("form")({
+    // width: '100%',
+    display: "flex",
+    flexWrap: "wrap",
+  });
 
   return (
     <>
@@ -105,9 +50,9 @@ export const ArchitectureSettingsListItem = () => {
         timeout="auto"
         unmountOnExit
       >
-        <form className={classes.container} noValidate autoComplete="off">
+        <StyledForm noValidate autoComplete="off">
           <ArchitectureSettingsGrid />
-        </form>
+        </StyledForm>
       </Collapse>
     </>
   );
