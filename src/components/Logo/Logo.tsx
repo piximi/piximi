739a6b24224f31638160797c3c0eb999b0a828f8
Nodari Gogoberidze <gnodar01@gmail.com>
2022-01-15 19:34:26 -0500

    Logo: Refactor use of makeStyles to use of sx prop

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { useStyles } from "./Logo.css";
 import { Typography } from "@mui/material";
 
 export const Logo = () => {
-  const classes = useStyles();
-
   return (
-    <Typography className={classes.title} color="inherit" noWrap variant="h6">
+    <Typography
+      sx={(theme) => ({
+        display: "none",
+        [theme.breakpoints.up("sm")]: {
+          display: "block",
+        },
+      })}
+      color="inherit"
+      noWrap
+      variant="h6"
+    >
       Piximi
     </Typography>
   );
