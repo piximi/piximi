d306bf6528624ed8046fb49bb7f2eec62c230029
Nodari Gogoberidze <gnodar01@gmail.com>
2022-01-22 14:33:19 -0500

    Enable routing with HashRouter

--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import { StyledEngineProvider } from "@mui/material/styles";
 import { MainView } from "../MainView";
-// import { AnnotatorView } from "../annotator";
+import { AnnotatorView } from "../annotator";
 import { usePreferredTheme } from "hooks/useTheme/usePreferredTheme";
 
 export const Application = () => {
@@ -13,12 +14,14 @@ export const Application = () => {
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
         <MainView />
-        {/* <BrowserRouter>
+        {/* <BrowserRouter> */}
+        <HashRouter>
           <Routes>
             <Route path="/" element={<MainView />} />
             <Route path="annotator" element={<AnnotatorView />} />
           </Routes>
-        </BrowserRouter> */}
+        </HashRouter>
+        {/* </BrowserRouter> */}
       </ThemeProvider>
     </StyledEngineProvider>
   );
