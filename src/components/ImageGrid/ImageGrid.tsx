3d1e4acdc8dddd6dfaad27d4b89ee83609a67929
Nodar Gogoberidze <gnodar01@gmail.com>
2022-02-25 12:29:16 -0500

    Memoize each grid item

--- src/components/ImageGrid/ImageGrid.tsx
+++ src/components/ImageGrid/ImageGrid.tsx
@@ -17,18 +17,81 @@ import {
 import { DropTargetMonitor, useDrop } from "react-dnd";
 import { NativeTypes } from "react-dnd-html5-backend";
 import { ImageIconLabel } from "./ImageIconLabel";
+import { memo } from "react";
 
 type ImageGridProps = {
   onDrop: (item: { files: any[] }) => void;
 };
 
-export const ImageGrid = ({ onDrop }: ImageGridProps) => {
+type ImageGridItemProps = {
+  image: ImageType;
+  selected: boolean;
+};
+
+const ImageGridItem = memo(({ image, selected }: ImageGridItemProps) => {
   const dispatch = useDispatch();
+  const scaleFactor = useSelector(tileSizeSelector);
 
+  const getSize = (scaleFactor: number) => {
+    const width = (220 * scaleFactor).toString() + "px";
+    const height = (220 * scaleFactor).toString() + "px";
+
+    return {
+      width: width,
+      height: height,
+      margin: "2px",
+    };
+  };
+
+  const getSelectionStatus = () => {
+    return selected
+      ? { border: "solid 5px green", borderRadius: "6px" }
+      : { border: "none" };
+  };
+
+  const onSelectImage = (image: ImageType) => {
+    if (selected) {
+      dispatch(applicationSlice.actions.deselectImage({ id: image.id }));
+    } else {
+      dispatch(applicationSlice.actions.selectImage({ id: image.id }));
+    }
+  };
+
+  return (
+    <ImageListItem
+      onClick={() => onSelectImage(image)}
+      style={getSize(scaleFactor)}
+      sx={getSelectionStatus()}
+    >
+      <Box
+        component="img"
+        alt=""
+        src={image.src}
+        sx={{
+          width: "100%",
+          height: "100%",
+          objectFit: "contain",
+          top: 0,
+          transform: "none",
+        }}
+      />
+
+      <ImageListItemBar
+        position="top"
+        actionIcon={<ImageIconLabel image={image} />}
+        actionPosition="left"
+        sx={{
+          background: "transparent",
+        }}
+      />
+    </ImageListItem>
+  );
+});
+
+export const ImageGrid = ({ onDrop }: ImageGridProps) => {
   const images = useSelector(visibleImagesSelector);
-
-  const selectedImages: Array<string> = useSelector(selectedImagesSelector);
-
+  const selectedImages = useSelector(selectedImagesSelector);
+  const scaleFactor = useSelector(tileSizeSelector);
   const max_images = 1000; //number of images from the project that we'll show
 
   const [{ isOver }, drop] = useDrop(
@@ -46,33 +109,6 @@ export const ImageGrid = ({ onDrop }: ImageGridProps) => {
     }),
     []
   );
-
-  const onSelectImage = (image: ImageType) => {
-    if (selectedImages.includes(image.id)) {
-      dispatch(applicationSlice.actions.deselectImage({ id: image.id }));
-    } else {
-      dispatch(applicationSlice.actions.selectImage({ id: image.id }));
-    }
-  };
-
-  const scaleFactor = useSelector(tileSizeSelector);
-
-  const getSize = (scaleFactor: number) => {
-    const width = (220 * scaleFactor).toString() + "px";
-    const height = (220 * scaleFactor).toString() + "px";
-
-    return {
-      width: width,
-      height: height,
-      margin: "2px",
-    };
-  };
-
-  const getSelectionStatus = (imageId: string) => {
-    return selectedImages.includes(imageId)
-      ? { border: "solid 2px blue", borderRadius: "3px" }
-      : { border: "none" };
-  };
 
   return (
     <Box
@@ -106,34 +142,11 @@ export const ImageGrid = ({ onDrop }: ImageGridProps) => {
           rowHeight={"auto"}
         >
           {images.slice(0, max_images).map((image: ImageType) => (
-            <ImageListItem
+            <ImageGridItem
+              image={image}
+              selected={selectedImages.includes(image.id)}
               key={image.id}
-              onClick={() => onSelectImage(image)}
-              style={getSize(scaleFactor)}
-              sx={getSelectionStatus(image.id)}
-            >
-              <Box
-                component="img"
-                alt=""
-                src={image.src}
-                sx={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "contain",
-                  top: 0,
-                  transform: "none",
-                }}
-              />
-
-              <ImageListItemBar
-                position="top"
-                actionIcon={<ImageIconLabel image={image} />}
-                actionPosition="left"
-                sx={{
-                  background: "transparent",
-                }}
-              />
-            </ImageListItem>
+            />
           ))}
         </ImageList>
 
