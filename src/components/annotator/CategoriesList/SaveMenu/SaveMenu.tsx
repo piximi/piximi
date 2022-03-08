eb9211d9276a3b38672b46206d8a5763ada45ba0
Levin Moser <lmoser@broadinstitute.org>
2022-03-08 16:37:46 -0500

    implement SaveAnnotationProject dialog

--- src/components/annotator/CategoriesList/SaveMenu/SaveMenu.tsx
+++ src/components/annotator/CategoriesList/SaveMenu/SaveMenu.tsx
@@ -1,13 +1,14 @@
 import { Menu } from "@mui/material";
 import { bindMenu } from "material-ui-popup-state";
 import React from "react";
-import { SaveProjectFileMenuItem } from "./SaveProjectFileMenuItem";
+import { SaveAnnotationProjectDialog } from "./SaveAnnotationProjectDialog";
 import { ExportAnnotationsAsLabeledInstancesMenuItem } from "./ExportAnnotationsAsLabeledInstancesMenuItem";
 import { ExportAnnotationsAsMatrixMenuItem } from "./ExportAnnotationsAsMatrixMenuItem";
 import { ExportAnnotationsAsLabeledSemanticMasksMenuItem } from "./ExportAnnotationsAsLabeledSemanticMasksMenuItem";
 import MenuItem from "@mui/material/MenuItem";
 import { ExportAnnotationsAsBinarySemanticMasksMenuItem } from "./ExportAnnotationsAsBinarySemanticMasksMenuItem";
 import { ExportAnnotationsAsBinaryInstancesMenuItem } from "./ExportAnnotationsAsBinaryInstancesMenuItem";
+import { useDialog } from "hooks/useDialog/useDialog";
 
 type SaveMenuProps = {
   popupState: any;
@@ -15,6 +16,8 @@ type SaveMenuProps = {
 
 export const SaveMenu = ({ popupState }: SaveMenuProps) => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const { onClose, onOpen, open } = useDialog();
 
   const handleClick = (event: React.MouseEvent<HTMLLIElement>) => {
     setAnchorEl(event.currentTarget);
@@ -25,40 +28,45 @@ export const SaveMenu = ({ popupState }: SaveMenuProps) => {
   };
 
   return (
-    <Menu {...bindMenu(popupState)}>
-      <MenuItem onClick={handleClick}>Export annotations as</MenuItem>
-      <Menu
-        id="save-annotations-as-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <ExportAnnotationsAsLabeledInstancesMenuItem
-          popupState={popupState}
-          handleCloseMenu={handleClose}
-        />
-        <ExportAnnotationsAsBinaryInstancesMenuItem
-          popupState={popupState}
-          handleCloseMenu={handleClose}
-        />
-        <ExportAnnotationsAsLabeledSemanticMasksMenuItem
-          popupState={popupState}
-          handleCloseMenu={handleClose}
-        />
-        <ExportAnnotationsAsBinarySemanticMasksMenuItem
-          popupState={popupState}
-          handleCloseMenu={handleClose}
-        />
-        <ExportAnnotationsAsMatrixMenuItem
-          popupState={popupState}
-          handleCloseMenu={handleClose}
-        />
+    <div>
+      <Menu {...bindMenu(popupState)}>
+        <MenuItem onClick={handleClick}>Export annotations as</MenuItem>
+        <Menu
+          id="save-annotations-as-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <ExportAnnotationsAsLabeledInstancesMenuItem
+            popupState={popupState}
+            handleCloseMenu={handleClose}
+          />
+          <ExportAnnotationsAsBinaryInstancesMenuItem
+            popupState={popupState}
+            handleCloseMenu={handleClose}
+          />
+          <ExportAnnotationsAsLabeledSemanticMasksMenuItem
+            popupState={popupState}
+            handleCloseMenu={handleClose}
+          />
+          <ExportAnnotationsAsBinarySemanticMasksMenuItem
+            popupState={popupState}
+            handleCloseMenu={handleClose}
+          />
+          <ExportAnnotationsAsMatrixMenuItem
+            popupState={popupState}
+            handleCloseMenu={handleClose}
+          />
+        </Menu>
+        <MenuItem onClick={onOpen}>Save Annotation Project</MenuItem>
       </Menu>
-      <SaveProjectFileMenuItem
+
+      <SaveAnnotationProjectDialog
+        onClose={onClose}
+        open={open}
         popupState={popupState}
-        handleCloseMenu={handleClose}
       />
-    </Menu>
+    </div>
   );
 };
