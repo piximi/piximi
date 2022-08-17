51d0edc1696331eb6c0d4fec37507772891a669f
Nodar Gogoberidze <gnodar01@gmail.com>
2022-08-17 17:26:11 -0400

    Update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,11 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
 }
