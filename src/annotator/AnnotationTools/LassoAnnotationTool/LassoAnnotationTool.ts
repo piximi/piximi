5b54fbf49186747f1f3cd732465a65bdbd6bd9f8
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2022-09-14 16:38:26 -0400

    [add, mod, wip] adds testing to annotator tools.

--- src/annotator/AnnotationTools/LassoAnnotationTool/LassoAnnotationTool.ts
+++ src/annotator/AnnotationTools/LassoAnnotationTool/LassoAnnotationTool.ts
@@ -24,7 +24,7 @@ export class LassoAnnotationTool extends AnnotationTool {
   onMouseDown(position: { x: number; y: number }) {
     if (this.annotationState === AnnotationStateType.Annotated) return;
 
-    if (this.buffer && this.buffer.length === 0) {
+    if (this.buffer.length === 0) {
       if (!this.origin) {
         this.origin = position;
         this.buffer = [...this.buffer, position.x, position.y];
@@ -60,9 +60,12 @@ export class LassoAnnotationTool extends AnnotationTool {
     if (this.annotationState !== AnnotationStateType.Annotating || !this.origin)
       return;
 
-    if (this.buffer.length < 3) return;
+    if (this.buffer.length < 3) {
+      console.log("Bufferr too small, goodbye");
+      return;
+    }
 
-    if (this.origin && this.buffer && this.buffer.length > 0) {
+    if (this.origin && this.buffer.length >= 3 && this.connected(position)) {
       this.buffer = [
         ...this.buffer,
         position.x,
@@ -76,7 +79,11 @@ export class LassoAnnotationTool extends AnnotationTool {
       this._boundingBox = this.computeBoundingBoxFromContours(this.points);
 
       const maskImage = this.computeAnnotationMaskFromPoints();
-      if (!maskImage) return;
+
+      if (!maskImage) {
+        console.log("I will exit, no mask");
+        return;
+      }
 
       this._mask = encode(maskImage.data);
 
@@ -86,6 +93,7 @@ export class LassoAnnotationTool extends AnnotationTool {
     }
 
     if (this.anchor) {
+      console.log("I will define an anchor");
       this.anchor = {
         x: this.buffer[this.buffer.length - 2],
         y: this.buffer[this.buffer.length - 1],
@@ -93,8 +101,9 @@ export class LassoAnnotationTool extends AnnotationTool {
 
       return;
     }
-
+    console.log("John Cena", this.origin, this.buffer.length);
     if (this.origin && this.buffer && this.buffer.length > 0) {
+      console.log("I will define an anchor");
       this.anchor = {
         x: this.buffer[this.buffer.length - 2],
         y: this.buffer[this.buffer.length - 1],
@@ -102,5 +111,18 @@ export class LassoAnnotationTool extends AnnotationTool {
 
       return;
     }
+  }
+  private connected(
+    position: { x: number; y: number },
+    threshold: number = 4
+  ): boolean | undefined {
+    if (!this.origin) return undefined;
+
+    const distance = Math.hypot(
+      position.x - this.origin.x,
+      position.y - this.origin.y
+    );
+
+    return distance < threshold;
   }
 }
