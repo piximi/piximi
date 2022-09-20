d36f8ed7cd31b6736ca7629e470e8e4ffc1b48ff
Nodar Gogoberidze <gnodar01@gmail.com>
2022-09-20 12:55:36 -0400

    [clean] Clean up unused imports
    resolves #380

--- src/components/annotator/ImageContent/Stage/Stage/SoundEvents.tsx
+++ src/components/annotator/ImageContent/Stage/Stage/SoundEvents.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { memo, useCallback, useEffect } from "react";
+import { memo } from "react";
 import useSound from "use-sound";
 import { useHotkeys } from "react-hotkeys-hook";
 
