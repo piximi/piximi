92e96749d810ac6e9cf2a3da068fa2d58f9682f9
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2022-09-21 14:07:00 -0400

    [del] removes unused variables

--- src/components/annotator/ImageContent/Stage/Stage/Stage.tsx
+++ src/components/annotator/ImageContent/Stage/Stage/Stage.tsx
@@ -43,10 +43,6 @@ import {
   annotationStateSelector,
   cursorSelector,
   setSelectedAnnotations,
-  activeImageIdSelector,
-  selectedAnnotationsSelector,
-  unselectedAnnotationsSelector,
-  annotatorImagesSelector,
 } from "store/image-viewer";
 import { selectedCategorySelector } from "store/common";
 
@@ -66,8 +62,6 @@ export const Stage = () => {
   const selectedAnnotationsIds = useSelector(selectedAnnotationsIdsSelector);
   const selectedCategory = useSelector(selectedCategorySelector);
 
-  const selectedAnnotations = useSelector(selectedAnnotationsSelector);
-  const unselectedAnnotations = useSelector(unselectedAnnotationsSelector);
   const selectionMode = useSelector(selectionModeSelector);
 
   const stageHeight = useSelector(stageHeightSelector);
@@ -77,9 +71,6 @@ export const Stage = () => {
   const saveLabelRef = useRef<Konva.Label>();
   const clearLabelRef = useRef<Konva.Label>();
 
-  const images = useSelector(annotatorImagesSelector);
-
-  const activeImageId = useSelector(activeImageIdSelector);
   const activeImagePlane = useSelector(activeImagePlaneSelector);
 
   const [currentPosition, setCurrentPosition] = useState<{
