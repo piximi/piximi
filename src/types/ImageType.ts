4d77286b6fd971bdd8d9b12e89f51183b1569018
Nodar Gogoberidze <gnodar01@gmail.com>
2022-09-28 12:24:21 -0400

    [wip, mod] Modified image type for tensor data

--- src/types/ImageType.ts
+++ src/types/ImageType.ts
@@ -1,20 +1,22 @@
+import { Tensor4D } from "@tensorflow/tfjs";
 import { Shape } from "./Shape";
 import { AnnotationType } from "./AnnotationType";
 import { Partition } from "./Partition";
+// TODO: image_data replaced with Colors
 import { Color } from "./Color";
+import { Colors } from "./tensorflow";
 
 export type ImageType = {
   annotations: Array<AnnotationType>;
   activePlane: number;
   categoryId: string;
-  colors: Array<Color>;
+  colors: Colors;
   id: string;
   name: string;
-  originalSrc: Array<Array<string>>; //We keep track of the URIs for each channel, for each slice (used for color adjustment)
+  data: Tensor4D; // [Z, H, W, C]
   partition: Partition;
   segmentationPartition?: Partition;
   visible: boolean;
-  shape: Shape;
   src: string; // The URI to be displayed on the canvas
 };
 
@@ -35,6 +37,21 @@ export type _ImageType = {
 };
 
 export type ShadowImageType = {
+  annotations: Array<AnnotationType>;
+  activePlane: number;
+  categoryId?: string;
+  colors: Array<Color>;
+  id: string;
+  name: string;
+  data?: Tensor4D; // [Z, H, W, C]
+  partition?: Partition;
+  segmentationPartition?: Partition;
+  visible?: boolean;
+  src: string; // The URI to be displayed on the canvas
+};
+
+//@TODO: image_data
+export type _ShadowImageType = {
   id: string;
   name: string;
   annotations: Array<AnnotationType>;
