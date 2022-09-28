03f56825e52e94fce40d95a77ac0a4069cd6919a
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2022-09-28 11:40:37 -0400

    [mod] adds testing for rectangular tool

--- src/annotator/AnnotationTools/RectangularAnnotationTool/RectangularAnnotationTool.ts
+++ src/annotator/AnnotationTools/RectangularAnnotationTool/RectangularAnnotationTool.ts
@@ -19,10 +19,8 @@ export class RectangularAnnotationTool extends AnnotationTool {
 
   onMouseDown(position: { x: number; y: number }) {
     if (this.annotationState === AnnotationStateType.Annotated) return;
-
     if (!this.width) {
       this.origin = position;
-
       this.setAnnotating();
     } else {
       this.resize(position);
@@ -41,13 +39,11 @@ export class RectangularAnnotationTool extends AnnotationTool {
 
   onMouseMove(position: { x: number; y: number }) {
     if (this.annotationState !== AnnotationStateType.Annotating) return;
-
     this.resize(position);
   }
 
   onMouseUp(position: { x: number; y: number }) {
     if (this.annotationState !== AnnotationStateType.Annotating) return;
-
     if (this.width) {
       this.points = this.convertToPoints();
 
