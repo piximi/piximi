9fb0d9fd5f287918753296678c90e18b55f6ef2e
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2022-09-28 11:40:37 -0400

    [mod] adds testing for rectangular tool

--- src/annotator/AnnotationTools/RectangularAnnotationTool/RectangularAnnotationTool.test.ts
+++ src/annotator/AnnotationTools/RectangularAnnotationTool/RectangularAnnotationTool.test.ts
@@ -1,130 +1,154 @@
 import { test } from "@jest/globals";
+import { Image } from "image-js";
+import { data } from "../testData.json";
+import { RectangularAnnotationTool } from "./RectangularAnnotationTool";
+import { AnnotationStateType, Category } from "types";
 
-test("deselect", () => {
-  expect(true).toBe(true);
+const src = data.image;
+
+test("onMouseDown", async () => {
+  const image = await Image.load(src);
+
+  const operator = new RectangularAnnotationTool(image);
+
+  operator.onMouseDown({ x: 0, y: 0 });
+
+  expect(operator.annotationState).toBe(AnnotationStateType.Annotating);
+
+  expect(operator.annotation).toBe(undefined);
+
+  expect(operator.origin).toStrictEqual({ x: 0, y: 0 });
+
+  expect(operator.width).toBe(undefined);
+  expect(operator.height).toBe(undefined);
 });
 
-// test("deselect", () => {
-//   const image = new ImageJS.Image(224, 224);
-//
-//   const selectionOperator = new RectangularAnnotationTool(image);
-//
-//   selectionOperator.annotated = true;
-//
-//   selectionOperator.origin = { x: 0, y: 0 };
-//
-//   selectionOperator.width = 100;
-//   selectionOperator.height = 100;
-//
-//   selectionOperator.deselect();
-//
-//   expect(selectionOperator.annotated).toBe(false);
-//   expect(selectionOperator.annotating).toBe(false);
-//
-//   expect(selectionOperator.annotation).toBe(undefined);
-//
-//   expect(selectionOperator.origin).toStrictEqual(undefined);
-//
-//   expect(selectionOperator.width).toBe(undefined);
-//   expect(selectionOperator.height).toBe(undefined);
-// });
-//
-// test("onMouseDown", () => {
-//   const image = new ImageJS.Image(224, 224);
-//
-//   const selectionOperator = new RectangularAnnotationTool(image);
-//
-//   selectionOperator.onMouseDown({ x: 0, y: 0 });
-//
-//   expect(selectionOperator.annotated).toBe(false);
-//   expect(selectionOperator.annotating).toBe(true);
-//
-//   expect(selectionOperator.annotation).toBe(undefined);
-//
-//   expect(selectionOperator.origin).toStrictEqual({ x: 0, y: 0 });
-//
-//   expect(selectionOperator.width).toBe(undefined);
-//   expect(selectionOperator.height).toBe(undefined);
-// });
-//
-// test("onMouseMove", () => {
-//   const image = new ImageJS.Image(224, 224);
-//
-//   const selectionOperator = new RectangularAnnotationTool(image);
-//
-//   selectionOperator.annotating = true;
-//
-//   selectionOperator.origin = { x: 0, y: 0 };
-//
-//   selectionOperator.onMouseMove({ x: 100, y: 100 });
-//
-//   expect(selectionOperator.annotated).toBe(false);
-//   expect(selectionOperator.annotating).toBe(true);
-//
-//   expect(selectionOperator.annotation).toBe(undefined);
-//
-//   expect(selectionOperator.origin).toStrictEqual({ x: 0, y: 0 });
-//
-//   expect(selectionOperator.width).toBe(100);
-//   expect(selectionOperator.height).toBe(100);
-// });
-//
-// test("onMouseUp", () => {
-//   const image = new ImageJS.Image(224, 224);
-//
-//   const selectionOperator = new RectangularAnnotationTool(image);
-//
-//   selectionOperator.annotating = true;
-//
-//   selectionOperator.origin = { x: 0, y: 0 };
-//
-//   selectionOperator.onMouseUp({ x: 100, y: 100 });
-//
-//   expect(selectionOperator.annotated).toBe(true);
-//   expect(selectionOperator.annotating).toBe(false);
-//
-//   expect(selectionOperator.annotation).toBe(undefined);
-//
-//   expect(selectionOperator.origin).toStrictEqual({ x: 0, y: 0 });
-//
-//   expect(selectionOperator.width).toBe(100);
-//   expect(selectionOperator.height).toBe(100);
-// });
-//
-// test("select", () => {
-//   const image = new ImageJS.Image(224, 224);
-//
-//   const selectionOperator = new RectangularAnnotationTool(image);
-//
-//   selectionOperator.annotated = true;
-//
-//   selectionOperator.origin = { x: 0, y: 0 };
-//
-//   selectionOperator.width = 100;
-//   selectionOperator.height = 100;
-//
-//   const category: Category = {
-//     color: "#0000FF",
-//     id: "5ed3511d-1223-4bba-a0c2-2b3897232d98",
-//     name: "foo",
-//     visible: true,
-//   };
-//   selectionOperator.annotate(category);
-//
-//   expect(selectionOperator.annotated).toBe(true);
-//   expect(selectionOperator.annotating).toBe(false);
-//
-//   expect(selectionOperator.annotation).toStrictEqual({
-//     boundingBox: [0, 0, 100, 100],
-//     categoryId: "5ed3511d-1223-4bba-a0c2-2b3897232d98",
-//     mask: "mask",
-//   });
-//
-//   expect(selectionOperator.boundingBox).toStrictEqual([0, 0, 100, 100]);
-//   expect(selectionOperator.mask).toBe("mask");
-//
-//   expect(selectionOperator.origin).toStrictEqual({ x: 0, y: 0 });
-//
-//   expect(selectionOperator.width).toBe(100);
-//   expect(selectionOperator.height).toBe(100);
-// });
+test("onMouseMove", async () => {
+  const image = await Image.load(src);
+
+  const operator = new RectangularAnnotationTool(image);
+
+  operator.onMouseDown({ x: 0, y: 0 });
+
+  operator.onMouseMove({ x: 100, y: 100 });
+
+  expect(operator.annotationState).toBe(AnnotationStateType.Annotating);
+
+  expect(operator.annotation).toBe(undefined);
+
+  expect(operator.origin).toStrictEqual({ x: 0, y: 0 });
+
+  expect(operator.width).toBe(100);
+  expect(operator.height).toBe(100);
+});
+
+test("onMouseUp", async () => {
+  const image = await Image.load(src);
+
+  const operator = new RectangularAnnotationTool(image);
+
+  operator.onMouseDown({ x: 0, y: 0 });
+
+  operator.onMouseMove({ x: 100, y: 100 });
+  operator.onMouseUp({ x: 100, y: 100 });
+  expect(operator.annotationState).toBe(AnnotationStateType.Annotated);
+
+  expect(operator.annotation).toBe(undefined);
+
+  expect(operator.origin).toStrictEqual({ x: 0, y: 0 });
+
+  expect(operator.width).toBe(100);
+  expect(operator.height).toBe(100);
+
+  expect(operator.points).toStrictEqual([0, 0, 100, 100]);
+  expect(operator.boundingBox).toStrictEqual([0, 0, 100, 100]);
+  expect(operator.mask).toBeDefined();
+});
+
+test("make rectangle by clicking twice", async () => {
+  const image = await Image.load(src);
+
+  const operator = new RectangularAnnotationTool(image);
+
+  operator.onMouseDown({ x: 0, y: 0 });
+  operator.onMouseUp({ x: 0, y: 0 });
+
+  operator.onMouseMove({ x: 100, y: 100 });
+  operator.onMouseDown({ x: 100, y: 100 });
+  operator.onMouseUp({ x: 100, y: 100 });
+  expect(operator.annotationState).toBe(AnnotationStateType.Annotated);
+
+  expect(operator.annotation).toBe(undefined);
+
+  expect(operator.origin).toStrictEqual({ x: 0, y: 0 });
+
+  expect(operator.width).toBe(100);
+  expect(operator.height).toBe(100);
+
+  expect(operator.points).toStrictEqual([0, 0, 100, 100]);
+  expect(operator.boundingBox).toStrictEqual([0, 0, 100, 100]);
+  expect(operator.mask).toBeDefined();
+});
+
+test("select", async () => {
+  const image = await Image.load(src);
+
+  const operator = new RectangularAnnotationTool(image);
+
+  operator.onMouseDown({ x: 0, y: 0 });
+
+  operator.onMouseMove({ x: 100, y: 100 });
+  operator.onMouseUp({ x: 100, y: 100 });
+
+  const category: Category = {
+    color: "#0000FF",
+    id: "5ed3511d-1223-4bba-a0c2-2b3897232d98",
+    name: "foo",
+    visible: true,
+  };
+  operator.annotate(category, 1);
+
+  expect(operator.annotationState).toBe(AnnotationStateType.Annotated);
+
+  expect(operator.origin).toStrictEqual({ x: 0, y: 0 });
+
+  expect(operator.width).toBe(100);
+  expect(operator.height).toBe(100);
+
+  expect(operator.points).toStrictEqual([0, 0, 100, 100]);
+  expect(operator.boundingBox).toStrictEqual([0, 0, 100, 100]);
+  expect(operator.mask).toBeDefined();
+
+  expect(operator.annotation).toMatchObject({
+    boundingBox: [0, 0, 100, 100],
+    categoryId: "5ed3511d-1223-4bba-a0c2-2b3897232d98",
+    plane: 1,
+  });
+});
+
+test("deselect", async () => {
+  const image = await Image.load(src);
+
+  const operator = new RectangularAnnotationTool(image);
+
+  operator.onMouseDown({ x: 0, y: 0 });
+
+  operator.onMouseMove({ x: 100, y: 100 });
+  operator.onMouseUp({ x: 100, y: 100 });
+
+  const category: Category = {
+    color: "#0000FF",
+    id: "5ed3511d-1223-4bba-a0c2-2b3897232d98",
+    name: "foo",
+    visible: true,
+  };
+  operator.annotate(category, 1);
+  operator.deselect();
+
+  expect(operator.annotationState).toBe(AnnotationStateType.Blank);
+
+  expect(operator.origin).toStrictEqual(undefined);
+
+  expect(operator.width).toBe(undefined);
+  expect(operator.height).toBe(undefined);
+});
