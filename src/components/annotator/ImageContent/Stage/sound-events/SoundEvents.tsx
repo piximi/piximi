835100fb364398443180c2866a3857ce11af6fac
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2022-11-08 16:15:04 -0500

    [add] Moves and adds new files

---
+++ src/components/annotator/ImageContent/Stage/sound-events/SoundEvents.tsx
@@ -0,0 +1,53 @@
+import { useSelector } from "react-redux";
+import { memo } from "react";
+import useSound from "use-sound";
+import { useHotkeys } from "hooks";
+
+import { soundEnabledSelector } from "store/image-viewer";
+import createAnnotationSoundEffect from "annotator/sounds/pop-up-on.mp3";
+import deleteAnnotationSoundEffect from "annotator/sounds/pop-up-off.mp3";
+import { HotkeyView } from "types";
+
+/*
+  This component is responsible for playing sound effects
+  following a hotkey.
+  Logic cannot be moved to Stage because the useSound
+  hook starts up an AudioContext, but AudioContexts
+  must be resumed, or created, only after a user Gesture.
+  Hence this component must only be mounted as a response to
+  some (arbitrary) user gesture, like onMouseDown.
+  See: https://goo.gl/7K7WLu
+  Technically useSound only *creates* an AudioContext,
+  and we only *play* the sounds after a user gesture,
+  so it would (and did) still work if this was in the Stage component,
+  but Chrome is overly aggressive and throws multiple warnings in Console,
+  so this component was made just to get rid of the annoying warnings.
+ */
+export const SoundEvents = memo(() => {
+  const soundEnabled = useSelector(soundEnabledSelector);
+
+  const [playCreateAnnotationSoundEffect, { sound: creeateAnnotationSound }] =
+    useSound(createAnnotationSoundEffect, { soundEnabled });
+  const [playDeleteAnnotationSoundEffect, { sound: deleteAnnotationSound }] =
+    useSound(deleteAnnotationSoundEffect, { soundEnabled });
+
+  useHotkeys(
+    "enter",
+    () => {
+      playCreateAnnotationSoundEffect();
+    },
+    HotkeyView.Annotator,
+    [soundEnabled, creeateAnnotationSound]
+  );
+
+  useHotkeys(
+    "escape, backspace, delete",
+    () => {
+      playDeleteAnnotationSoundEffect();
+    },
+    HotkeyView.Annotator,
+    [soundEnabled, deleteAnnotationSound]
+  );
+
+  return <></>;
+});
---
+++ src/components/annotator/ImageContent/Stage/sound-events/index.ts
@@ -0,0 +1,1 @@
+export { SoundEvents } from "./SoundEvents";
