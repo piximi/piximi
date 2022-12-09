30ac5c597334045512c52031539c0749fd2919ac
Andrea Papaleo <apapale1@binghamton.edu>
2022-12-09 16:41:23 -0500

    [mod, add] Adds hook for image offset and aligns annotations in stage

--- src/components/annotator/Stage/ColorAnnotationToolTip/ColorAnnotationToolTip.tsx
+++ src/components/annotator/Stage/ColorAnnotationToolTip/ColorAnnotationToolTip.tsx
@@ -2,29 +2,25 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import * as ReactKonva from "react-konva";
 
-import {
-  annotationStateSelector,
-  stageScaleSelector,
-  toolTypeSelector,
-} from "store/annotator";
+import { annotationStateSelector, toolTypeSelector } from "store/annotator";
 
 import { AnnotationStateType, ToolType } from "types";
 
 type ColorAnnotationToolTipProps = {
   toolTipPosition?: { x: number; y: number };
   initialPosition?: { x: number; y: number };
+  imageOrigin?: { x: number; y: number };
   tolerance: number;
 };
 
 export const ColorAnnotationToolTip = ({
   toolTipPosition,
   initialPosition,
+  imageOrigin,
   tolerance,
 }: ColorAnnotationToolTipProps) => {
   const [text, setText] = useState<string>("Tolerance: 0%");
   const toolType = useSelector(toolTypeSelector);
-
-  const stageScale = useSelector(stageScaleSelector);
 
   const annotationState = useSelector(annotationStateSelector);
 
@@ -49,18 +45,17 @@ export const ColorAnnotationToolTip = ({
         <ReactKonva.Line
           points={[
             toolTipPosition.x,
-            toolTipPosition.y!,
-            initialPosition.x,
-            initialPosition.y,
+            toolTipPosition.y,
+            initialPosition.x + imageOrigin!.x,
+            initialPosition.y + imageOrigin!.y,
           ]}
-          scale={{ x: stageScale, y: stageScale }}
           strokeWidth={1}
           stroke="white"
         />
         <ReactKonva.Label
           position={{
-            x: toolTipPosition.x * stageScale,
-            y: toolTipPosition.y * stageScale,
+            x: toolTipPosition.x, //+ imageOrigin!.x,
+            y: toolTipPosition.y, //+ imageOrigin!.y,
           }}
           opacity={0.75}
         >
