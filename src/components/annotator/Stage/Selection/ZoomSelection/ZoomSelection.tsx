ef5b3c2110d710703d08c7ab5509fc1bb06ce8f5
Andrea Papaleo <apapale1@binghamton.edu>
2022-12-14 15:08:07 -0500

    [mod] Fixes selection zooming

--- src/components/annotator/Stage/Selection/ZoomSelection/ZoomSelection.tsx
+++ src/components/annotator/Stage/Selection/ZoomSelection/ZoomSelection.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 import * as ReactKonva from "react-konva";
 
-import { zoomSelectionSelector } from "store/annotator";
+import { stageScaleSelector, zoomSelectionSelector } from "store/annotator";
 
 export const ZoomSelection = () => {
   const { dragging, minimum, maximum, selecting } = useSelector(
     zoomSelectionSelector
   );
+  const stageScale = useSelector(stageScaleSelector);
 
   if (!minimum || !maximum || !selecting || !dragging) return <></>;
 
@@ -15,20 +16,20 @@ export const ZoomSelection = () => {
     <>
       <ReactKonva.Group>
         <ReactKonva.Rect
-          dash={[4, 2]}
+          dash={[4 / stageScale, 2 / stageScale]}
           height={maximum.y - minimum.y}
           stroke="black"
-          strokeWidth={1}
+          strokeWidth={1 / stageScale}
           width={maximum.x - minimum.x}
           x={minimum.x}
           y={minimum.y}
         />
         <ReactKonva.Rect
-          dash={[4, 2]}
+          dash={[4 / stageScale, 2 / stageScale]}
           //dashOffset={-dashOffset}
           height={maximum.y - minimum.y}
           stroke="white"
-          strokeWidth={1}
+          strokeWidth={1 / stageScale}
           width={maximum.x - minimum.x}
           x={minimum.x}
           y={minimum.y}
