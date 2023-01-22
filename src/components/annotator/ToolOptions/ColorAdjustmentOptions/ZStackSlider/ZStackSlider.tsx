7df5e4fdb7aff11f9502daddf202f03eb215d7e3
Nodari Gogoberidze <gnodar01@gmail.com>
2023-01-22 08:14:05 -0500

    [clean] Clean up prerendering

--- src/components/annotator/ToolOptions/ColorAdjustmentOptions/ZStackSlider/ZStackSlider.tsx
+++ src/components/annotator/ToolOptions/ColorAdjustmentOptions/ZStackSlider/ZStackSlider.tsx
@@ -4,29 +4,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { List, ListItem, ListItemText, Slider } from "@mui/material";
 
 import { activeImageSelector } from "store/common";
-import {
-  activeImagePlaneSelector,
-  activeImageRenderedSrcsSelector,
-  imageViewerSlice,
-} from "store/image-viewer";
+import { activeImagePlaneSelector, imageViewerSlice } from "store/image-viewer";
 
 export const ZStackSlider = () => {
   const dispatch = useDispatch();
   const activeImage = useSelector(activeImageSelector);
   const activePlane = useSelector(activeImagePlaneSelector);
-  const renderedSrcs = useSelector(activeImageRenderedSrcsSelector);
 
   if (!activeImage || activeImage!.shape.planes === 1)
     return <React.Fragment />;
 
   const handleChange = async (event: Event, newValue: number | number[]) => {
     if (typeof newValue === "number") {
-      if (renderedSrcs.length === 0) return;
-      dispatch(
-        imageViewerSlice.actions.setImageSrc({
-          src: renderedSrcs[newValue],
-        })
-      );
       dispatch(
         imageViewerSlice.actions.setActiveImagePlane({
           activeImagePlane: newValue,
