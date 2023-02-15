f806ba2dabe03db435fb8576b3a92ebfa425eca2
Nodar Gogoberidze <gnodar01@gmail.com>
2023-02-15 14:47:15 -0500

    [add] Add COCO type checking

--- src/types/runtime/SerializedFileType.ts
+++ src/types/runtime/SerializedFileType.ts
@@ -2,6 +2,61 @@ import * as T from "io-ts";
 import { getOrElseW } from "fp-ts/Either";
 import { failure } from "io-ts/PathReporter";
 
+//#region COCO Serialization Type
+
+const SerializedCOCOInfoRtype = T.type({
+  year: T.Integer,
+  version: T.string,
+  description: T.string,
+  contributor: T.string,
+  url: T.string,
+  date_created: T.string,
+});
+
+const SerializedCOCOImageRType = T.type({
+  id: T.Integer,
+  width: T.Integer,
+  height: T.Integer,
+  file_name: T.string,
+  license: T.Integer,
+  flickr_url: T.string,
+  coco_url: T.string,
+  date_captured: T.string,
+});
+
+const SerializedCOCOLicenseRType = T.type({
+  id: T.Integer,
+  name: T.string,
+  url: T.string,
+});
+
+const SerializedCOCOAnnotationRType = T.type({
+  id: T.Integer,
+  image_id: T.Integer,
+  category_id: T.Integer,
+  segmentation: T.array(T.array(T.number)),
+  area: T.number,
+  bbox: T.tuple([T.number, T.number, T.number, T.number]),
+});
+
+const SerializedCOCOCategoryRType = T.type({
+  id: T.Integer,
+  name: T.string,
+  supercategory: T.string,
+});
+
+export const SerializedCOCOFileRType = T.type({
+  info: SerializedCOCOInfoRtype,
+  images: T.array(SerializedCOCOImageRType),
+  annotations: T.array(SerializedCOCOAnnotationRType),
+  licenses: T.array(SerializedCOCOLicenseRType),
+  categories: T.array(SerializedCOCOCategoryRType),
+});
+
+//#endregion COCO Serialization Type
+
+//#region Basic Serialization Type
+
 const SerializedCategoryRType = T.type({
   id: T.string,
   color: T.string, // 3 byte hex, eg "#a08cd2"
@@ -21,6 +76,8 @@ export const SerializedFileRType = T.type({
   categories: T.array(SerializedCategoryRType),
   annotations: T.array(SerializedAnnotationRType),
 });
+
+//#endregion Basic Serialization Type
 
 const toError = (errors: any) => {
   throw new Error(failure(errors).join("\n"));
