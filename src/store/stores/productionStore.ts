42d82724a6dc3f08d11b41db92b095df97e0b360
Nodar Gogoberidze <gnodar01@gmail.com>
2023-03-01 17:47:56 -0500

    [add] Add saga monitor

--- src/store/stores/productionStore.ts
+++ src/store/stores/productionStore.ts
@@ -6,11 +6,34 @@ import {
   StoreEnhancer,
 } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import createSagaMonitor from "@clarketm/saga-monitor";
 
 import { reducer } from "../reducer";
 import { rootSaga } from "../sagas";
 
-export const saga = createSagaMiddleware();
+const sagaMonitorConfig = {
+  level: "debug", // logging level
+  verbose: true, // verbose mode
+  color: "#03A9F4", // default color
+  rootSagaStart: true, // show root saga start effect
+  effectTrigger: true, // show triggered effects
+  effectResolve: true, // show resolved effects
+  effectReject: true, // show rejected effects
+  effectCancel: true, // show cancelled effects
+  actionDispatch: true, // show dispatched actions
+};
+
+export const saga = createSagaMiddleware(
+  process.env.NODE_ENV !== "production"
+    ? {
+        onError: (err, errInfo) => console.error(err, errInfo),
+        sagaMonitor:
+          process.env.REACT_APP_LOG_LEVEL === "5"
+            ? createSagaMonitor(sagaMonitorConfig)
+            : undefined,
+      }
+    : undefined
+);
 
 const enhancers: StoreEnhancer[] = [];
 
---
+++ src/store/stores/saga-monitor.d.ts
@@ -0,0 +1,1 @@
+declare module "@clarketm/saga-monitor";
