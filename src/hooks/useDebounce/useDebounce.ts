51d27ed5049a48ddf4024add30263036a85ab3b8
Andrea Papaleo <apapale1@binghamton.edu>
2023-03-06 11:46:14 -0500

    [mod] Format with prettier

--- src/hooks/useDebounce/useDebounce.ts
+++ src/hooks/useDebounce/useDebounce.ts
@@ -1,31 +1,29 @@
 import { useEffect, useRef } from "react";
 
-type PassedFunc = (...args:any[]) => void;
-type Timer = ReturnType<typeof setTimeout>
+type PassedFunc = (...args: any[]) => void;
+type Timer = ReturnType<typeof setTimeout>;
 
 export function useDebounce<Func extends PassedFunc>(
-  func:Func,
+  func: Func,
   delay: number
 ) {
   // State and setters for debounced value
- const timer = useRef<Timer>()
+  const timer = useRef<Timer>();
 
- useEffect(() => {
-   return () => {
-     if (!timer.current) return;
-     clearTimeout(timer.current);
-   };
- }, []);
+  useEffect(() => {
+    return () => {
+      if (!timer.current) return;
+      clearTimeout(timer.current);
+    };
+  }, []);
 
- const debouncedFunction = ((...args) => {
-  const newTimer = setTimeout(()=>{
-    func(...args);
-  }, delay);
-  clearTimeout(timer.current);
-  timer.current = newTimer;
- }) as Func
-
- 
+  const debouncedFunction = ((...args) => {
+    const newTimer = setTimeout(() => {
+      func(...args);
+    }, delay);
+    clearTimeout(timer.current);
+    timer.current = newTimer;
+  }) as Func;
 
   return debouncedFunction;
 }
