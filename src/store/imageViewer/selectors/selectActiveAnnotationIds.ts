49e526b422cb2a007b8e915a7ccf6aef1643bc23
Andrea Papaleo <apapale1@binghamton.edu>
2023-06-08 15:34:56 +0200

    [fix] hide/show annotations

--- src/store/imageViewer/selectors/selectActiveAnnotationIds.ts
+++ src/store/imageViewer/selectors/selectActiveAnnotationIds.ts
@@ -1,14 +1,14 @@
 import { createSelector } from "@reduxjs/toolkit";
-import { activeImageIdSelector } from "./activeImageIdSelector";
-import { selectAnnotationsByImageDict } from "store/data/selectors/annotation/annotationSelectors";
 
-export const selectActiveAnnotationIds = createSelector(
-  [activeImageIdSelector, selectAnnotationsByImageDict],
-  (imageId, annotationsByImage) => {
-    if (!imageId) return [];
-    return annotationsByImage[imageId];
-  }
-);
+import { ImageViewerStore } from "types";
+
+export const selectActiveAnnotationIds = ({
+  imageViewer,
+}: {
+  imageViewer: ImageViewerStore;
+}) => {
+  return imageViewer.activeAnnotationIds;
+};
 
 export const selectActiveAnnotationIdsCount = createSelector(
   selectActiveAnnotationIds,
