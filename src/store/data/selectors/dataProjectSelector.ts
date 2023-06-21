5aa2ccce07f33573807cc8936c57a89c2a2a630d
Andrea Papaleo <apapale1@binghamton.edu>
2023-06-21 12:30:02 -0400

    [fix] fixes error when deleting image in annotator

--- src/store/data/selectors/dataProjectSelector.ts
+++ src/store/data/selectors/dataProjectSelector.ts
@@ -1,34 +1,47 @@
-import { Category, DataStoreSlice, OldImageType } from "types";
+import { createSelector } from "@reduxjs/toolkit";
+import { Category, OldImageType } from "types";
+import { selectAllImages } from "./image";
+import { selectAllImageCategories } from "./image-category/imageCategorySelectors";
+import {
+  selectAllAnnotations,
+  selectAnnotationsByImageDict,
+} from "./annotation";
+import { selectAllAnnotationCategories } from "./annotation-category";
 
-export const dataProjectSelector = ({
-  data,
-}: {
-  data: DataStoreSlice;
-}): {
-  images: Array<OldImageType>;
-  categories: Array<Category>;
-  annotationCategories: Array<Category>;
-} => {
-  const images: Array<OldImageType> = Object.values(data.images.entities).map(
-    (image) => {
-      const annotationIds = data.annotationsByImage[image.saved.id];
-      const annotations = Object.values(data.annotations.entities).filter(
-        (annotation) => annotationIds.includes(annotation!.saved.id)
+export const dataProjectSelector = createSelector(
+  [
+    selectAllImages,
+    selectAllImageCategories,
+    selectAllAnnotations,
+    selectAllAnnotationCategories,
+    selectAnnotationsByImageDict,
+  ],
+  (
+    allImages,
+    allImageCategories,
+    allAnnotations,
+    allAnnotationCategories,
+    annotationsByImage
+  ): {
+    images: Array<OldImageType>;
+    categories: Array<Category>;
+    annotationCategories: Array<Category>;
+  } => {
+    const images: Array<OldImageType> = allImages.map((image) => {
+      const annotationIds = annotationsByImage[image.id];
+      const annotations = allAnnotations.filter((annotation) =>
+        annotationIds.includes(annotation.id)
       );
       return {
-        ...image.saved,
-        annotations: annotations.map((entity) => entity.saved),
+        ...image,
+        annotations,
       } as OldImageType;
-    }
-  );
+    });
 
-  return {
-    images,
-    categories: Object.values(data.imageCategories.entities).map(
-      (entity) => entity.saved
-    ),
-    annotationCategories: Object.values(data.annotationCategories.entities).map(
-      (entity) => entity.saved
-    ),
-  };
-};
+    return {
+      images,
+      categories: allImageCategories,
+      annotationCategories: allAnnotationCategories,
+    };
+  }
+);
