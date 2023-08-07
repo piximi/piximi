2cbcd8a356fa4740d15e47006386e3ea3fb16bec
Nodar Gogoberidze <gnodar01@gmail.com>
2023-08-07 16:47:12 -0400

    [add] Switch example project hosting
    - replace LFS h5 urls to S3 zipped zarr urls
    - create packages.json prepare script to download zipped zarr for dev mode

---
+++ prepare.js
@@ -0,0 +1,42 @@
+const https = require("https");
+const fs = require("fs");
+
+const projectPath = "src/data/exampleProjects";
+
+const projectNames = [
+  "BBBC013ExampleProject",
+  "mnistExampleProject",
+  "cElegansExampleProject",
+  "HumanU2OSCellsExampleProject",
+  "PLP1ExampleProject",
+];
+
+// CloudFront distribution domain
+const domain = "https://dw9hr7pc3ofrm.cloudfront.net";
+// S3 bucket path
+const rootPath = "exampleProjects";
+const ext = "zip";
+
+const download = function (url, dest, cb) {
+  const file = fs.createWriteStream(dest);
+  https.get(url, function (response) {
+    response.pipe(file);
+    file.on("finish", function () {
+      file.close(cb);
+    });
+  });
+};
+
+const writeIfMissing = async function () {
+  for (const name of projectNames) {
+    const local = `${projectPath}/${name}.${ext}`;
+    const remote = `${domain}/${rootPath}/${name}.${ext}`;
+    if (!fs.existsSync(local)) {
+      download(remote, local, () => console.log(`wrote ${local}`));
+    } else {
+      console.log(`${local} exists`);
+    }
+  }
+};
+
+writeIfMissing();
