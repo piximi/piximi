9df56cf466f54d2980462ed85d0ac3b8035bbe7b
Andrea Papaleo <apapale1@binghamton.edu>
2024-02-21 12:05:28 -0500

    usr-stry - user filters images

---
+++ src/components/drawers/ImageToolDrawer/tool-options-drawer/FilterOptions/FilterOptionsNew.tsx
@@ -0,0 +1,72 @@
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Stack } from "@mui/material";
+import { CategoryFilterListNew } from "./CategoryFilterListNew";
+import { projectSlice } from "store/slices/project";
+import { Partition } from "types";
+import { selectActiveThingFilters } from "store/slices/project/selectors";
+import { PartitionFilterListNew } from "./PartitionFilterListNew";
+
+export const FilterOptionsNew = () => {
+  const thingFilters = useSelector(selectActiveThingFilters);
+  const dispatch = useDispatch();
+
+  const toggleImageCategoryFilter = useCallback(
+    (categoryId: string) => {
+      if (
+        thingFilters.categoryId &&
+        thingFilters.categoryId.includes(categoryId)
+      ) {
+        dispatch(
+          projectSlice.actions.removeThingCategoryFilters({
+            categoryIds: [categoryId],
+          })
+        );
+      } else {
+        dispatch(
+          projectSlice.actions.addThingCategoryFilters({
+            categoryIds: [categoryId],
+          })
+        );
+      }
+    },
+    [dispatch, thingFilters.categoryId]
+  );
+
+  const toggleThingPartition = useCallback(
+    (partition: Partition) => {
+      if (
+        thingFilters.partition &&
+        thingFilters.partition.includes(partition)
+      ) {
+        dispatch(
+          projectSlice.actions.removeThingPartitionFilters({
+            partitions: [partition],
+          })
+        );
+      } else {
+        dispatch(
+          projectSlice.actions.addThingPartitionFilters({
+            partitions: [partition],
+          })
+        );
+      }
+    },
+    [dispatch, thingFilters.partition]
+  );
+
+  return (
+    <Stack maxWidth="100%">
+      <CategoryFilterListNew
+        header="Image Categories"
+        filteredCategories={thingFilters.categoryId ?? []}
+        toggleFilter={toggleImageCategoryFilter}
+      />
+      <PartitionFilterListNew
+        header="Image Partitions"
+        filteredPartitions={thingFilters.partition ?? []}
+        toggleFilter={toggleThingPartition}
+      />
+    </Stack>
+  );
+};
---
+++ src/store/slices/project/selectors/selectUnfilteredPartitionIds.ts
@@ -0,0 +1,14 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { selectActiveThingFilters } from "../selectors";
+import { difference } from "lodash";
+import { Partition } from "types";
+
+export const selectUnfilteredActivePartitions = createSelector(
+  selectActiveThingFilters,
+  (thingFilters) => {
+    const filteredPartitions = thingFilters.partition;
+    const allPartitions = Object.values(Partition);
+    const unfilteredPartitions = difference(allPartitions, filteredPartitions);
+    return unfilteredPartitions;
+  }
+);
