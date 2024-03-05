34c281e541cd6eeb316a39e96513eb8b1f26752f
Andrea Papaleo <apapale1@binghamton.edu>
2024-03-05 18:17:04 -0500

    user predicts with stardistVHE

--- src/types/ModelType.ts
+++ src/types/ModelType.ts
@@ -3,6 +3,7 @@ import { Segmenter } from "utils/common/models/AbstractSegmenter/AbstractSegment
 import { MobileNet } from "utils/common/models/MobileNet/MobileNet";
 import { SimpleCNN } from "utils/common/models/SimpleCNN/SimpleCNN";
 import { StardistVHE } from "utils/common/models/StardistVHE/StardistVHE";
+import { StardistVHENew } from "utils/common/models/StardistVHE/StardistVHENew";
 import { FullyConvolutionalSegmenter } from "utils/common/models/FullyConvolutionalSegmenter/FullyConvolutionalSegmenter";
 import { CocoSSD } from "utils/common/models/CocoSSD/CocoSSD";
 import { Cellpose } from "utils/common/models/Cellpose/Cellpose";
@@ -35,4 +36,5 @@ export const availableSegmenterModels: Array<Segmenter> = [
   new StardistVHE(),
   new Cellpose(),
   new CellposeNew(),
+  new StardistVHENew(),
 ];
