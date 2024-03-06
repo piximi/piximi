c3437cd3fe4f7648a3809095a341222e6220d8f9
Andrea Papaleo <apapale1@binghamton.edu>
2024-03-06 16:55:26 -0500

    refactor file

--- scripts/checkConsoleLogs.sh
+++ scripts/checkConsoleLogs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-exitcode=0
-consoleregexp='console\.log'
-files=`git diff --cached --name-only | grep -E "^src"`
-for file in $files
+EXITCODE=0
+PTRN='console\.log'
+FILES=$(git diff --cached --name-only | grep -E "^src")
+for file in $FILES
 do
     echo $file
-    if [ $(git diff --cached $file | grep -E "$consoleregexp" | wc -l) != 0 ]
+    if [ $(git diff --cached $file | grep -E "$PTRN" | wc -l) != 0 ]
     then 
-        cat $file | grep -n -E "$consoleregexp"
-        exitcode=1
+        cat $file | grep -n -E "$PTRN"
+        EXITCODE=1
  
     fi
 done
-exit $exitcode
+exit $EXITCODE
 # CHECK
