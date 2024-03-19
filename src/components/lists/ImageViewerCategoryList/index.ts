88873c55dae67e64c03ca849787c0f9615db88d9
Andrea Papaleo <apapale1@binghamton.edu>
2024-03-19 09:53:37 -0100

    user filters annotations

---
+++ src/components/lists/ImageViewerCategoryList/index.ts
@@ -0,0 +1,1 @@
+export { ImageViewerCategoryList } from "./ImageViewerCategoryList";
--- src/store/slices/imageViewer/selectors/selectFilteredAnnotationCategoryIds.ts
+++ src/store/slices/imageViewer/selectors/selectFilteredAnnotationCategoryIds.ts
@@ -7,3 +7,11 @@ export const selectFilteredAnnotationCategoryIds = ({
 }) => {
   return imageViewer.annotationFilters.categoryId;
 };
+
+export const selectFilteredImageViewerCategoryIds = ({
+  imageViewer,
+}: {
+  imageViewer: ImageViewer;
+}) => {
+  return imageViewer.filters.categoryId;
+};
