abad2836049ea609ab883556ade4aaa119e87d84
Andrea Papaleo <apapale1@binghamton.edu>
2024-03-26 14:43:11 -0400

    updates color adjuster

--- src/store/slices/imageViewer/reselectors.ts
+++ src/store/slices/imageViewer/reselectors.ts
@@ -3,6 +3,8 @@ import { selectThingsDictionary } from "store/slices/newData/selectors/selectors
 import { NewImageType } from "types/ImageType";
 import { selectActiveImageId } from "./selectors/selectActiveImageId";
 import { selectImageStackImageIds } from "./selectors/selectImageStackImageIds";
+import { Colors, ColorsRaw } from "types";
+import { generateBlankColors } from "utils/common/image";
 
 export const selectActiveImage = createSelector(
   selectActiveImageId,
@@ -28,5 +30,24 @@ export const selectImageViewerImages = createSelector(
       []
     );
     return imageViewerImages;
+  }
+);
+
+export const selectActiveImageRawColor = createSelector(
+  selectActiveImage,
+  (image): ColorsRaw => {
+    let colors: Colors;
+    if (!image) {
+      colors = generateBlankColors(3);
+    } else {
+      colors = image.colors;
+    }
+
+    return {
+      // is sync appropriate? if so we may need to dispose??
+      color: colors.color.arraySync() as [number, number, number][],
+      range: colors.range,
+      visible: colors.visible,
+    };
   }
 );
