d77cf7be3c51459f3508b57fbaefd51d8697e8c7
Andrea Papaleo <apapale1@binghamton.edu>
2024-04-19 13:31:54 -0400

    revert names

--- src/views/Application/Application.tsx
+++ src/views/Application/Application.tsx
@@ -14,7 +14,7 @@ export const Application = () => {
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
-        <BrowserRouter basename={"/"}>
+        <BrowserRouter basename={"/piximi-beta"}>
           <Routes>
             <Route path="/" element={<ProjectViewer />} />
             <Route path="imageviewer" element={<ImageViewer />} />
