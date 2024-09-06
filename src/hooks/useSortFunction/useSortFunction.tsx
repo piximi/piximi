9ac8edc06b017c1ee548073d4fe8ab4ad80d3435
Nodar Gogoberidze <gnodar01@gmail.com>
2024-09-06 17:11:05 -0400

    [fix] Re-randomize sort only on sort key change
    resolves #523

--- src/hooks/useSortFunction/useSortFunction.tsx
+++ src/hooks/useSortFunction/useSortFunction.tsx
@@ -5,6 +5,28 @@ import { Thing } from "store/data/types";
 import { selectSortType } from "store/project/selectors";
 import { ThingSortKey } from "utils/common/enums";
 
+// uuid -> numerical value (determenistic)
+const hash = (id: Thing["id"]) => {
+  let hashValue = 0;
+  for (let i = 0; i < id.length; i++) {
+    hashValue = (hashValue << 5) - hashValue + id.charCodeAt(i);
+    hashValue |= 0; // Convert to 32-bit integer
+  }
+  return hashValue;
+};
+
+// taken from https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
+// okay to use because security is not a concern for this use-case
+const splitmix32 = (seed: number) => {
+  seed |= 0;
+  seed = (seed + 0x9e3779b9) | 0;
+  let t = seed ^ (seed >>> 16);
+  t = Math.imul(t, 0x21f0aaad);
+  t = t ^ (t >>> 15);
+  t = Math.imul(t, 0x735a2d97);
+  return ((t = t ^ (t >>> 15)) >>> 0) / 4294967296;
+};
+
 export const useSortFunction = () => {
   const sortType = useSelector(selectSortType);
   const [previousSortType, setPreviousSortType] = useState<ThingSortKey>(
@@ -20,6 +42,7 @@ export const useSortFunction = () => {
 
   useEffect(() => {
     if (sortType !== previousSortType && sortType !== ThingSortKey.Category) {
+      const randomSeed = Math.floor(Math.random() * 100);
       setPreviousSortType(sortType);
       switch (sortType) {
         case ThingSortKey.FileName:
@@ -29,10 +52,13 @@ export const useSortFunction = () => {
           break;
 
         case ThingSortKey.Random:
-          setSortFunction(
-            () => (a: Thing, b: Thing) =>
-              Math.round(Math.random() * 10) >= 5 ? 1 : -1
-          );
+          setSortFunction(() => (a: Thing, b: Thing) => {
+            const aHash = hash(a.name);
+            const bHash = hash(b.name);
+            const aVal = splitmix32(aHash + randomSeed);
+            const bVal = splitmix32(bHash + randomSeed);
+            return aVal - bVal;
+          });
           break;
         case ThingSortKey.Name:
           setSortFunction(
