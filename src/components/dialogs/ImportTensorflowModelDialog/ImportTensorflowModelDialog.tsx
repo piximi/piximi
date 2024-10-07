aff2434ee86437588f164b22ddd3fbf21b156c56
Beth Cimini <bethac07@users.noreply.github.com>
2024-10-07 06:52:57 -0500

    Make Cellpose warning more explicit

--- src/components/dialogs/ImportTensorflowModelDialog/ImportTensorflowModelDialog.tsx
+++ src/components/dialogs/ImportTensorflowModelDialog/ImportTensorflowModelDialog.tsx
@@ -199,7 +199,7 @@ export const ImportTensorflowModelDialog = ({
           }
           sx={{ mb: 2 }}
         >
-          This model performs inference in the cloud ☁️
+          This model performs inference in the cloud ☁️; images will leave your machine. 
         </Alert>
       </Collapse>
       <DialogTitle>
