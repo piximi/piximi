5a0a504bd90dd3fd6a32ef151099e38fdd5a9317
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2024-10-17 01:06:59 -0400

    File reorg

---
+++ src/components/dialogs/SettingsDialog/SettingsItem.tsx
@@ -0,0 +1,24 @@
+import { Box, Typography } from "@mui/material";
+import React, { ReactNode } from "react";
+
+export const SettingsItem = ({
+  title,
+  children,
+}: {
+  title: string | ReactNode;
+  children: ReactNode;
+}) => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="flex-end"
+      height="40px"
+    >
+      {typeof title === "string" ? <Typography>{title}</Typography> : title}
+
+      {children}
+    </Box>
+  );
+};
