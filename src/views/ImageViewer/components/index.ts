b6657920c39ec1f31039dab1bd4d7c6ed06abac7
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2024-12-17 22:56:49 -0500

    refactor/isolates -image-viwer-store

--- src/views/ImageViewer/components/index.ts
+++ src/views/ImageViewer/components/index.ts
@@ -2,5 +2,3 @@ export { ImageViewerCategories } from "./ImageViewerCategories";
 export { CustomToolTip } from "./CustomToolTip";
 export { ExportAnnotationsMenu } from "./ExportAnnotationsMenu";
 export { ExportAnnotationsListItem } from "./ExportAnnotationsListItem";
-export { ApplyColorsButton } from "./ApplyColorsButton";
-export { ChannelsList } from "./ChannelsList";
