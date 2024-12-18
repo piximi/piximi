cf3b38b54a7dd78272d71d241d29c0a1ee049dda
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2024-12-18 11:09:48 -0500

    refactor/moves stage context to view

--- src/views/ImageViewer/sections/Stage/Selection/selection-options/EllipticalSelection.tsx
+++ src/views/ImageViewer/sections/Stage/Selection/selection-options/EllipticalSelection.tsx
@@ -4,7 +4,7 @@ import { Ellipse as KonvaEllipse, Group as KonvaGroup } from "react-konva";
 
 import { useMarchingAnts } from "../../../../hooks";
 
-import { StageContext } from "contexts";
+import { StageContext } from "views/ImageViewer/state/StageContext";
 import { selectImageOrigin } from "views/ImageViewer/state/imageViewer/selectors";
 
 import { EllipticalAnnotationTool } from "views/ImageViewer/utils/tools";
