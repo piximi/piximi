2a73254c8c5d950f3b8bf3b6f4173267d948ed2f
Nodar Gogoberidze <gnodar01@gmail.com>
2025-01-10 13:11:28 -0500

    [add] Add bump scripts
    - add npm script, and GH action to bump version automatically on merge
    - bumps patch if no labels
    - else label the PR to the type of bump desired (including nobump)

--- scripts/bump.js
+++ scripts/bump.js
@@ -2,12 +2,19 @@ const fs = require("fs");
 const semver = require("semver");
 
 const packageJsonPath = "./package.json";
-const bumpType = process.argv[2]; // 'patch', 'minor', 'major'
+let bumpType = process.argv[2]; // 'bump:patch', 'bump:minor', 'bump:major'
 
-if (!["patch", "minor", "major"].includes(bumpType)) {
-  console.error('Invalid bump type. Use "patch", "minor", or "major".');
+if (
+  !["bump:none", "bump:patch", "bump:minor", "bump:major"].includes(bumpType)
+) {
+  console.error(
+    'Invalid bump type. Use "bump:none", "bump:patch", "bump:minor", "bump:major".'
+  );
   process.exit(1);
 }
+
+// remove the "bump:" portion
+bumpType = bumpType.slice(bumpType.search(":") + 1);
 
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
 
@@ -17,17 +24,22 @@ if (!semver.valid(currentVersion)) {
   console.error("Invalid version in package.json:", currentVersion);
   process.exit(1);
 }
+if (bumpType === "none") {
+  console.log("Skipping version bump");
+} else {
+  const newVersion = semver.inc(currentVersion, bumpType);
 
-const newVersion = semver.inc(currentVersion, bumpType);
+  console.log(`Bumping version: ${currentVersion} -> ${newVersion}`);
 
-console.log(`Bumping version: ${currentVersion} -> ${newVersion}`);
+  packageJson.version = newVersion;
 
-packageJson.version = newVersion;
+  fs.writeFileSync(
+    packageJsonPath,
+    JSON.stringify(packageJson, null, 2) + "\n",
+    "utf-8"
+  );
 
-fs.writeFileSync(
-  packageJsonPath,
-  JSON.stringify(packageJson, null, 2) + "\n",
-  "utf-8"
-);
+  console.log("Updated package.json with new version:", newVersion);
+}
 
-console.log("Updated package.json with new version:", newVersion);
+process.exit(0);
