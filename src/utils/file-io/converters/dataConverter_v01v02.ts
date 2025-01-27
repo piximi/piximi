502ab760f896afb03d169e1ac58ae9767f163133
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2025-01-26 21:30:03 -0500

    [wip] post-rebase fixes

--- src/utils/file-io/converters/dataConverter_v01v02.ts
+++ src/utils/file-io/converters/dataConverter_v01v02.ts
@@ -141,7 +141,6 @@ export const dataConverter_v01v02 = (data: {
   const numAnnotationsOfKindPerImage: Record<string, number> = {};
 
   for (const annotation of annotations as AnnotationObject[]) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { plane: _plane, ..._annotation } = {
       ...annotation,
       activePlane: annotation.plane,
@@ -187,7 +186,7 @@ export const dataConverter_v01v02 = (data: {
         }
         return shape;
       },
-      { planes: 0, height: 0, width: 0, channels: 0 },
+      { planes: 0, height: 0, width: 0, channels: 0 }
     );
     _annotation.partition = Partition.Unassigned;
     _annotation.bitDepth = things.entities[_annotation.imageId]!.bitDepth;
@@ -207,7 +206,7 @@ export const convertAnnotationsWithExistingProject_v01v02 = async (
   existingImages: Record<string, ImageObject>,
   existingKinds: Record<string, Kind>,
   oldAnnotations: OldAnnotationType[],
-  oldAnnotationCategories: OldCategory[],
+  oldAnnotationCategories: OldCategory[]
 ) => {
   const catId2Name: Record<string, string> = {};
   const newKinds: Record<string, Kind> = {};
@@ -246,7 +245,7 @@ export const convertAnnotationsWithExistingProject_v01v02 = async (
     newAnn.categoryId = kind.unknownCategoryId;
     const numAnns = intersection(
       existingImage.containing,
-      kind.containing,
+      kind.containing
     ).length;
     newAnn.name = `${existingImage.name}-${kind.id}_${numAnns}`;
     let renderedIm: Image;
@@ -259,7 +258,7 @@ export const convertAnnotationsWithExistingProject_v01v02 = async (
     const imageProperties = getPropertiesFromImageSync(
       renderedIm,
       existingImage,
-      ann,
+      ann
     );
     Object.assign(newAnn, imageProperties);
     newAnn.shape = convertArrayToShape(newAnn.data!.shape as ShapeArray);
