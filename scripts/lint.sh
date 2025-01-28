c708096cfa789cd0d8e8d91b873c062df2d6cec3
Nodar Gogoberidze <gnodar01@gmail.com>
2025-01-28 09:28:34 -0500

    Add lint logging

---
+++ scripts/lint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pnpm eslint --max-warnings 0 --debug 2>&1 | grep  "eslint:eslint Lint " | awk '{print $NF}' | while IFS= read -r line; do
+    printf "\033[K" # clear from cursor to end of line
+    echo $line
+    printf "\033[F" # cursor beginning of preivous line
+    sleep 0.01  # Adjust speed of updates
+done
