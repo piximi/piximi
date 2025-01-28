dd98f3d8aea251f49fe07a3a4815bed4b974bc2d
Nodar Gogoberidze <gnodar01@gmail.com>
2025-01-28 11:16:11 -0500

    [wip] Add formatting scripts to package.json

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pnpm eslint --max-warnings 0 --debug 2>&1 | grep  "eslint:eslint Lint " | awk '{print $NF}' | while IFS= read -r line; do
+pnpm eslint --max-warnings 0 --debug "$@" 2>&1 | grep  "eslint:eslint Lint " | awk '{print $NF}' | while IFS= read -r line; do
     printf "\033[K" # clear from cursor to end of line
     echo $line
     printf "\033[F" # cursor beginning of preivous line
