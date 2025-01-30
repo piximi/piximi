ff8eb55f312d1290f2c429b3beef08579d4e043b
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2025-01-30 10:54:40 -0500

    adds and removes icons -updates annotation mode tooltip titles

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,17 +1,17 @@
-// Import and export icons
-
 // Import and export components
 export { ColorAdjustment } from "./ColorAdjustment";
 export { ColorAnnotation } from "./ColorAnnotation";
 export { EllipticalAnnotation } from "./EllipticalAnnotation";
 export { FreehandAnnotation } from "./FreehandAnnotation";
-export { Hand } from "./Hand";
 export { LassoAnnotation } from "./LassoAnnotation";
 export { MagneticAnnotation } from "./MagneticAnnotation";
-export { ObjectAnnotation } from "./ObjectAnnotation";
 export { PolygonAnnotation } from "./PolygonAnnotation";
 export { QuickAnnotation } from "./QuickAnnotation";
 export { RectangleAnnotation } from "./RectangleAnnotation";
 export { Selection } from "./Selection";
 
 export { CursorZoom, StageZoom, Zoom } from "./Zoom";
+export { SubtractAnnotationsIcon } from "./SubtractAnnotationsIcon";
+export { CombineAnnotationsIcon } from "./CombineAnnotationsIcon";
+export { IntersectAnnotationsIcon } from "./IntersectAnnotationsIcon";
+export { NewAnnotationIcon } from "./NewAnnotationIcon";
