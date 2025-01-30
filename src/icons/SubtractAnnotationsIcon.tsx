1390f1d183254be5ef6c580aabd6c16b1ea1076c
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2025-01-30 10:54:40 -0500

    [wip] adds and removes icons -updates annotation mode tooltip titles

---
+++ src/icons/SubtractAnnotationsIcon.tsx
@@ -0,0 +1,171 @@
+export const SubtractAnnotationsIcon = ({ color }: { color: string }) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      xmlns="http://www.w3.org/200/svg"
+      viewBox="0 0 24 24"
+    >
+      <path
+        id="top-left-1"
+        d="m 2 9.5 v -1.5 h 1.5"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="top-1"
+        d="m 6 8 h 6"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeDasharray=" 2 2"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="top-right-1"
+        d="m 16 9.5  v -1.5 h -1.5"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="right-1"
+        d="m 16 12 v 6 "
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeDasharray=" 2 2"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="bottom-right-1"
+        d="m 14.5 22 h 1.5 v -1.5"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="bottom-1"
+        d="m 6 22 h 6"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeDasharray=" 2 2"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="bottom-left-1"
+        d="m 2 20.5 v 1.5 h 1.5"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="left-1"
+        d="m 2 12 v 6 "
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeDasharray=" 2 2"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="top-left-2"
+        d="m 8 3.5 v -1.5 h 1.5"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="top-2"
+        d="m 12 2 h 6"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeDasharray=" 2 2"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="top-right-2"
+        d="m 22 3.5  v -1.5 h -1.5"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="right-2"
+        d="m 22 6 v 6 "
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeDasharray=" 2 2"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="bottom-right-2"
+        d="m 20.5 16 h 1.5 v -1.5"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="bottom-2"
+        d="m 12 16 h 6"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeDasharray=" 2 2"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="bottom-left-2"
+        d="m 8 14.5 v 1.5 h 1.5"
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="left-2"
+        d="m 8 6 v 6 "
+        fill="none"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeDasharray=" 2 2"
+        strokeLinejoin="round"
+      />
+
+      <path
+        id="top-left-1"
+        d="m 2 8 v 14 h 14 v -5.25 h -8.75 v -8.75 h -5.25 "
+        fill={color}
+        strokeWidth="1.5"
+        stroke={color}
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
