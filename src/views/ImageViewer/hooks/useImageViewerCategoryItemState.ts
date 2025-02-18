77c664447f051ef58183aa16174c004e10b16751
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2025-02-18 13:52:10 -0500

    [wip,fix] fixes improper category edit in image viewer - generalizes "CategoryDialog" - updates thing filter selector

--- src/views/ImageViewer/hooks/useImageViewerCategoryItemState.ts
+++ src/views/ImageViewer/hooks/useImageViewerCategoryItemState.ts
@@ -43,6 +43,14 @@ export const useImageViewerCategoryItemState = (category: Category) => {
     );
   };
 
+  const editCategory = (id: string, name: string, color: string) => {
+    dispatch(
+      annotatorSlice.actions.editCategory({
+        category: { id, name, color },
+      }),
+    );
+  };
+
   const clearObjects = (category: Category) => {
     // dispatch(
     //   imageViewerSlice.actions.removeActiveAnnotationIds({
@@ -95,6 +103,7 @@ export const useImageViewerCategoryItemState = (category: Category) => {
     objectCount,
     handleSelect,
     handleToggleCategoryVisibility,
+    editCategory,
     deleteCategory,
     clearObjects,
   };
