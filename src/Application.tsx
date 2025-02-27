90d4cd7a2514faa9d760366584412ba653041bf2
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2025-02-26 21:36:11 -0500

    replace loading screen with welcom page - closes #573

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,6 +1,5 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
 import { CssBaseline } from "@mui/material";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
 
@@ -9,76 +8,27 @@ import { usePreferredMuiTheme } from "hooks";
 import { ProjectViewer } from "views/ProjectViewer";
 import { ImageViewer } from "views/ImageViewer";
 import { MeasurementView } from "views/MeasurementView";
-import { LoadingScreen } from "./LoadingScreen";
+import { WelcomeScreen } from "./WelcomeScreen";
 
 import { FileUploadProvider } from "contexts";
-import { dataSlice } from "store/data";
-import { projectSlice } from "store/project";
-
-import { loadExampleImage } from "utils/file-io/loadExampleImage";
-import { dataConverter_v01v02 } from "utils/file-io/converters/dataConverter_v01v02";
-
-import { OldAnnotationType, OldCategory, OldImageType } from "store/data/types";
-import { SerializedFileType } from "utils/file-io/types";
-
-import { cellPaintingAnnotations } from "data/exampleImages";
-import colorImage from "images/cell-painting.png";
 
 export const Application = () => {
   const theme = usePreferredMuiTheme();
-  const dispatch = useDispatch();
-  const [hasLoaded, setHasLoaded] = useState(false);
-
-  const loadDataState = useCallback(async () => {
-    const { image, annotationCategories, annotations } =
-      (await loadExampleImage(
-        colorImage,
-        cellPaintingAnnotations as SerializedFileType,
-        // imageFile.name points to
-        // "/static/media/cell-painting.f118ef087853056f08e6.png"
-        "cell-painting.png",
-      )) as {
-        image: OldImageType;
-        annotationCategories: OldCategory[];
-        annotations: OldAnnotationType[];
-      };
-
-    const dataState = dataConverter_v01v02({
-      images: [image],
-      oldCategories: [],
-      annotations,
-      annotationCategories,
-    });
-
-    dispatch(dataSlice.actions.initializeState({ data: dataState }));
-
-    dispatch(projectSlice.actions.setProjectImageChannels({ channels: 3 }));
-    setHasLoaded(true);
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (!hasLoaded) {
-      loadDataState();
-    }
-  }, [loadDataState, hasLoaded]);
 
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {hasLoaded ? (
-          <FileUploadProvider>
-            <BrowserRouter basename={"/"}>
-              <Routes>
-                <Route path="/" element={<ProjectViewer />} />
-                <Route path="imageviewer" element={<ImageViewer />} />
-                <Route path="measurements" element={<MeasurementView />} />
-              </Routes>
-            </BrowserRouter>
-          </FileUploadProvider>
-        ) : (
-          <LoadingScreen />
-        )}
+        <FileUploadProvider>
+          <BrowserRouter basename={"/"}>
+            <Routes>
+              <Route path="/" element={<WelcomeScreen />} />
+              <Route path="project" element={<ProjectViewer />} />
+              <Route path="imageviewer" element={<ImageViewer />} />
+              <Route path="measurements" element={<MeasurementView />} />
+            </Routes>
+          </BrowserRouter>
+        </FileUploadProvider>
       </ThemeProvider>
     </StyledEngineProvider>
   );
