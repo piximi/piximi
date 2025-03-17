3363203670c690ab480043e2da8fcc160eba4c97
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2025-03-17 17:38:36 -0400

    [add, mod] adds priority help selection

--- src/contexts/HelpContext.tsx
+++ src/contexts/HelpContext.tsx
@@ -1,5 +1,5 @@
-import { Box, GlobalStyles } from "@mui/material";
-import { createContext, useContext, useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import { createContext, useContext, useState } from "react";
 import "./help.css";
 
 const HelpContext = createContext<null | {
@@ -10,25 +10,8 @@ const HelpContext = createContext<null | {
 export const HelpProvider = ({ children }: { children: React.ReactNode }) => {
   const [helpMode, setHelpMode] = useState(false);
 
-  useEffect(() => {
-    if (helpMode) {
-      document.body.className = "help-info";
-    } else {
-      document.body.className = "";
-    }
-  }, [helpMode]);
   return (
     <HelpContext.Provider value={{ helpMode, setHelpMode }}>
-      <GlobalStyles
-        styles={(theme) => ({
-          "html [data-help]": helpMode
-            ? {
-                outline: `2px dashed ${theme.palette.info.main}`,
-                "outline-offset": "-3px",
-              }
-            : {},
-        })}
-      />
       <Box>{children}</Box>
     </HelpContext.Provider>
   );
