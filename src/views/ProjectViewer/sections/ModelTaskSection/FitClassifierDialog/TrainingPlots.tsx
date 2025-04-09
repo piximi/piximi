7791c69e52002de245b349d658a4bc30ad956bc9
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2025-04-09 17:13:30 -0400

    [wip]

--- src/views/ProjectViewer/sections/ModelTaskSection/FitClassifierDialog/TrainingPlots.tsx
+++ src/views/ProjectViewer/sections/ModelTaskSection/FitClassifierDialog/TrainingPlots.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useClassifierHistory } from "views/ProjectViewer/contexts/ClassifierHistoryProvider";
 import { TwoDataPlot } from "../data-display";
 
 const y: {
@@ -6,15 +7,16 @@ const y: {
   y: number;
 }[] = [];
 const TrainingPlots = () => {
+  const { modelHistory } = useClassifierHistory();
   return (
     <div>
       <TwoDataPlot
         title="Training History - Accuracy per Epoch"
         yLabel="Accuracy"
         xLabel="Epoch"
-        yData1={y}
+        yData1={modelHistory.categoricalAccuracy}
         id1="Accuracy"
-        yData2={y}
+        yData2={modelHistory.val_categoricalAccuracy}
         id2="Validation Accuracy"
       />
 
@@ -22,9 +24,9 @@ const TrainingPlots = () => {
         title="Training History - Loss per Epoch"
         yLabel="Loss"
         xLabel="Epoch"
-        yData1={y}
+        yData1={modelHistory.loss}
         id1="Loss"
-        yData2={y}
+        yData2={modelHistory.val_loss}
         id2="Validation Loss"
         dynamicYRange={true}
       />
