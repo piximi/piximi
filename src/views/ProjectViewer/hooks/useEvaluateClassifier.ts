7e3da691a8b05954120f7688d092ee1d6a55fc9a
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2025-04-18 12:20:46 -0400

    [wip] completes extraction of model functions - begins work on HITL workflow - allows users to view previous training run evaluations

---
+++ src/views/ProjectViewer/hooks/useEvaluateClassifier.ts
@@ -0,0 +1,93 @@
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { applicationSettingsSlice } from "store/applicationSettings";
+import { AlertType } from "utils/common/enums";
+import { getStackTraceFromError } from "utils/common/helpers";
+import { AlertState } from "utils/common/types";
+import { useClassifierStatus } from "../contexts/ClassifierStatusProvider";
+import { ModelStatus } from "utils/models/enums";
+import { selectActiveKnownCategories } from "store/project/reselectors";
+import { selectClassifierModel } from "store/classifier/reselectors";
+import { classifierSlice } from "store/classifier";
+import { selectActiveKindId } from "store/project/selectors";
+
+export const useEvaluateClassifier = () => {
+  const dispatch = useDispatch();
+  const { modelStatus, setModelStatus } = useClassifierStatus();
+  const activeCategories = useSelector(selectActiveKnownCategories);
+  const activeKindId = useSelector(selectActiveKindId);
+  const selectedModel = useSelector(selectClassifierModel);
+  const handleError = useCallback(
+    async (error: Error, name: string, initialModelStatus?: ModelStatus) => {
+      const stackTrace = await getStackTraceFromError(error);
+      const alertState: AlertState = {
+        alertType: AlertType.Error,
+        name: name,
+        description: `${error.name}:\n${error.message}`,
+        stackTrace: stackTrace,
+      };
+      if (import.meta.env.NODE_ENV !== "production") {
+        console.error(
+          alertState.name,
+          "\n",
+          alertState.description,
+          "\n",
+          alertState.stackTrace,
+        );
+      }
+      dispatch(
+        applicationSettingsSlice.actions.updateAlertState({
+          alertState: alertState,
+        }),
+      );
+      setModelStatus(initialModelStatus ?? ModelStatus.Idle);
+    },
+    [dispatch],
+  );
+  const evaluateClassifier = async () => {
+    if (!selectedModel) return;
+    const initialModelStatus = modelStatus;
+    if (!selectedModel.validationLoaded) {
+      dispatch(
+        applicationSettingsSlice.actions.updateAlertState({
+          alertState: {
+            alertType: AlertType.Info,
+            name: "Validation set is empty",
+            description: "Cannot evaluate model on empty validation set.",
+          },
+        }),
+      );
+    } else if (selectedModel.numClasses !== activeCategories.length) {
+      dispatch(
+        applicationSettingsSlice.actions.updateAlertState({
+          alertState: {
+            alertType: AlertType.Warning,
+            name: "The output shape of your model does not correspond to the number of categories!",
+            description: `The trained model has an output shape of ${selectedModel.numClasses} but there are ${activeCategories.length} categories in  the project.\nMake sure these numbers match by retraining the model with the given setup or upload a corresponding new model.`,
+          },
+        }),
+      );
+    } else {
+      setModelStatus(ModelStatus.Evaluating);
+      try {
+        const evaluationResult = await selectedModel.evaluate();
+        dispatch(
+          classifierSlice.actions.updateEvaluationResult({
+            evaluationResult,
+            kindId: activeKindId,
+          }),
+        );
+      } catch (error) {
+        handleError(
+          error as Error,
+          "Error computing the evaluation results",
+          initialModelStatus,
+        );
+        return;
+      }
+    }
+
+    setModelStatus(initialModelStatus);
+  };
+  return evaluateClassifier;
+};
--- src/views/ProjectViewer/sections/ModelTaskSection/ClassifierSection.tsx
+++ src/views/ProjectViewer/sections/ModelTaskSection/ClassifierSection.tsx
@@ -1,10 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Stack, Typography } from "@mui/material";
-
+import {
+  alpha,
+  AppBar,
+  Box,
+  Container,
+  Dialog,
+  DialogContent,
+  IconButton,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { useDialog, useDialogHotkey } from "hooks";
-
-import { SaveFittedModelDialog } from "components/dialogs";
+import { Close } from "@mui/icons-material";
+import {
+  DialogTransitionSlide,
+  SaveFittedModelDialog,
+} from "components/dialogs";
 import { ModelExecButtonGroup } from "./ModelExecButtonGroup";
 import { ModelIOButtonGroup } from "./ModelIOButtonGroup";
 import { ImportTensorflowClassificationModelDialog } from "./ImportTensorflowModelDialog";
@@ -13,14 +29,24 @@ import { EvaluateClassifierDialog } from "./EvaluateClassifierDialog";
 
 import { HotkeyContext } from "utils/common/enums";
 import { ModelStatus, ModelTask } from "utils/models/enums";
-import { selectClassifierModel } from "store/classifier/reselectors";
+import {
+  selectClassifierEvaluationResult,
+  selectClassifierModel,
+} from "store/classifier/reselectors";
 import { selectActiveKindId } from "store/project/selectors";
 import { Model } from "utils/models/Model";
-import { Shape } from "store/data/types";
+import { Shape, Thing } from "store/data/types";
 import { SequentialClassifier } from "utils/models/classification";
 import { classifierSlice } from "store/classifier";
 import { PredictionListItems } from "views/ProjectViewer/components/list-items";
 import { useClassifierStatus } from "views/ProjectViewer/contexts/ClassifierStatusProvider";
+import { selectActiveThings } from "store/project/reselectors";
+import { APPLICATION_COLORS } from "utils/common/constants";
+import { useClassifierHistory } from "views/ProjectViewer/contexts/ClassifierHistoryProvider";
+import { orderBy } from "lodash";
+import { usePredictClassifier } from "views/ProjectViewer/hooks/usePredictClassifier";
+import { selectCategoriesDictionary } from "store/data/selectors";
+import { useEvaluateClassifier } from "views/ProjectViewer/hooks/useEvaluateClassifier";
 
 export const ClassifierSection = () => {
   const dispatch = useDispatch();
@@ -29,7 +55,9 @@ export const ClassifierSection = () => {
   const [waitingForResults, setWaitingForResults] = useState(false);
   const [helperText, setHelperText] = useState<string>("No trained model");
   const { modelStatus } = useClassifierStatus();
-
+  const predictClassifier = usePredictClassifier();
+  const evaluateClassifier = useEvaluateClassifier();
+  const evaluationResults = useSelector(selectClassifierEvaluationResult);
   const handleImportModel = async (model: Model, inputShape: Shape) => {
     if (model instanceof SequentialClassifier) {
       dispatch(
@@ -52,6 +80,11 @@ export const ClassifierSection = () => {
     onOpen: handleOpenEvaluateClassifierDialog,
     open: evaluateClassifierDialogOpen,
   } = useDialog();
+  const {
+    onClose: handleCloseHTLDialog,
+    //onOpen: handleOpenHTLDialog,
+    open: htlDialogOpen,
+  } = useDialog();
 
   const {
     onClose: handleCloseImportClassifierDialog,
@@ -68,16 +101,16 @@ export const ClassifierSection = () => {
     onOpen: handleOpenFitClassifierDialog,
     open: fitClassifierDialogOpen,
   } = useDialogHotkey(HotkeyContext.ClassifierDialog, false);
+
+  const handlePredict = async () => {
+    await predictClassifier();
+  };
+
   const handleEvaluate = async () => {
     if (!selectedModel) return;
+    if (selectedModel?.history.history.length > evaluationResults.length)
+      await evaluateClassifier();
 
-    const evaluationResult = await selectedModel.evaluate();
-    dispatch(
-      classifierSlice.actions.updateEvaluationResult({
-        evaluationResult,
-        kindId: activeKindId,
-      }),
-    );
     handleOpenEvaluateClassifierDialog();
   };
 
@@ -151,6 +184,7 @@ export const ClassifierSection = () => {
         <ModelExecButtonGroup
           modelStatus={modelStatus}
           handleFit={handleOpenFitClassifierDialog}
+          handlePredict={handlePredict}
           handleEvaluate={handleEvaluate}
           modelTrainable={!selectedModel || selectedModel.trainable}
           helperText={helperText}
@@ -174,11 +208,118 @@ export const ClassifierSection = () => {
         openedDialog={fitClassifierDialogOpen}
         closeDialog={handleCloseFitClassifierDialog}
       />
+      <HTLDialog
+        openedDialog={htlDialogOpen}
+        closeDialog={handleCloseHTLDialog}
+      />
 
       <EvaluateClassifierDialog
         openedDialog={evaluateClassifierDialogOpen}
         closeDialog={handleCloseEvaluateClassifierDialog}
       />
     </>
+  );
+};
+
+//NOTE: kind is passed as a prop and used internally instead of the kind returned
+// by the active kind selector to keep from rerendering the grid items when switching tabs
+export const ImageGrid = ({
+  things,
+}: {
+  things: Array<Thing & { probability: number }>;
+}) => {
+  const categories = useSelector(selectCategoriesDictionary);
+
+  return (
+    <Container maxWidth={false}>
+      <ImageList sx={{ width: "100%", height: "100%" }} cols={3} gap={24}>
+        {things.map((thing) => (
+          <ImageListItem key={thing.id} sx={{ width: "100%", height: "100%" }}>
+            <img src={thing.src} />
+            <ImageListItemBar
+              title={`Category: ${categories[thing.categoryId].name}`}
+              subtitle={`Confidence: ${(thing.probability * 100).toFixed(2)}`}
+              sx={{
+                backgroundColor: alpha(categories[thing.categoryId].color, 0.7),
+                color: (theme) =>
+                  theme.palette.getContrastText(
+                    categories[thing.categoryId].color,
+                  ),
+              }}
+              position="bottom"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Container>
+  );
+};
+
+type EvaluateClassifierDialogProps = {
+  closeDialog: () => void;
+  openedDialog: boolean;
+};
+
+const HTLDialog = ({
+  closeDialog,
+  openedDialog,
+}: EvaluateClassifierDialogProps) => {
+  const things = useSelector(selectActiveThings);
+  const { predictedProbabilities } = useClassifierHistory();
+
+  const predictedThings = useMemo(() => {
+    const predicted = things
+      .filter((thing) => Object.keys(predictedProbabilities).includes(thing.id))
+      .map((thing) => ({
+        ...thing,
+        probability: predictedProbabilities[thing.id],
+      }));
+    return orderBy(predicted, ["probability"], ["asc"]);
+  }, [predictedProbabilities, things]);
+  return (
+    <Dialog
+      onClose={closeDialog}
+      open={openedDialog}
+      fullWidth
+      maxWidth="md"
+      slots={{ transition: DialogTransitionSlide }}
+      sx={{ zIndex: 1203, height: "100%" }}
+    >
+      <AppBar
+        sx={{
+          position: "sticky",
+          backgroundColor: "transparent",
+          boxShadow: "none",
+          borderBottom: `1px solid ${APPLICATION_COLORS.borderColor}`,
+        }}
+      >
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <IconButton
+            edge="start"
+            color="primary"
+            onClick={closeDialog}
+            aria-label="Close"
+            sx={{
+              position: "absolute",
+              right: 8,
+              my: "auto",
+            }}
+          >
+            <Close />
+          </IconButton>
+
+          <Typography variant="h4">Predictions</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <DialogContent>
+        <ImageGrid things={predictedThings} />
+      </DialogContent>
+    </Dialog>
   );
 };
