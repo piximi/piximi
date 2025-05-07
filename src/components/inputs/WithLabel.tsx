1fbc2531bcdb5d0b64fa13f14d95f06905581b0f
Andrea Papaleo <apapaleo3@icloud.com>
2025-05-07 14:50:10 -0400

    update class map selection, model selection ui fix

--- src/components/inputs/WithLabel.tsx
+++ src/components/inputs/WithLabel.tsx
@@ -1,21 +1,26 @@
 import { Typography, TypographyProps, Stack, FormControl } from "@mui/material";
-import React from "react";
+import React, { CSSProperties } from "react";
 
 export const WithLabel = ({
   label,
   labelProps,
   children,
   fullWidth,
+  sx,
 }: {
   label: string;
   labelProps: Omit<TypographyProps, "children">;
   children: React.ReactChild;
   fullWidth?: boolean;
+  sx?: CSSProperties;
 }) => {
   return (
     <Stack
       direction="row"
-      sx={{ alignItems: "center", width: fullWidth ? "100%" : undefined }}
+      sx={{
+        ...{ alignItems: "center", width: fullWidth ? "100%" : undefined },
+        ...sx,
+      }}
     >
       <Typography {...labelProps}>{label}</Typography>
       <FormControl sx={fullWidth ? {} : { width: "auto" }} fullWidth>
