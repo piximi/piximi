b8169e86e4d8d989c555f9ce1358b5ad00f8500b
Andrea Papaleo <apapaleo3@icloud.com>
2025-05-17 23:02:54 -0400

    change app options from list items to icon buttons

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,6 +1,5 @@
 export { BaseAppDrawer } from "./BaseAppDrawer";
 export { CustomAppBar } from "./CustomAppBar";
 export { CustomTabs } from "./custom-tab-switcher";
-export { HelpDrawer } from "./HelpDrawer";
 export { DropBox } from "./DropBox";
 export { ToolTipTab } from "./TooltipTab";
