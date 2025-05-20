5b8414cc05066a100647900b2cf64e00e6f9d149
Andrea Papaleo <apapaleo3@icloud.com>
2025-05-20 10:33:22 -0400

    [mod] set project name when opening example image

--- src/views/ProjectViewer/components/dialogs/OpenExampleDialog/ExampleImageCard/ExampleImageCard.tsx
+++ src/views/ProjectViewer/components/dialogs/OpenExampleDialog/ExampleImageCard/ExampleImageCard.tsx
@@ -55,6 +55,9 @@ export const ExampleImageCard = ({
 
     batch(() => {
       dispatch(projectSlice.actions.resetProject());
+      dispatch(
+        projectSlice.actions.setProjectName({ name: exampleImage.name }),
+      );
       dispatch(projectSlice.actions.setProjectImageChannels({ channels: 3 }));
       dispatch(
         dataSlice.actions.initializeState({
