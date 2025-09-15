d9d030161148ed97b5867e1ab85fc68426eaea1c
Andrea Papaleo <apapaleo3@icloud.com>
2025-09-15 11:12:43 -0400

    [mod] fix (de)serialization

--- src/utils/file-io/enums.ts
+++ src/utils/file-io/enums.ts
@@ -20,9 +20,70 @@ export enum MIMETYPES {
   JPEG = "image/jpeg",
   TIFF = "image/tiff",
   TIF = "image/tif",
-  TIFF_APPLICATION = "application/tiff",
-  TIF_APPLICATION = "application/tif",
+  TIFF_Application = "application/tiff",
+  TIF_Application = "application/tif",
   DICOM = "image/dicom",
   BMP = "image/bmp",
   DICOM_APPLICATION = "application/dicom",
+}
+
+export enum METADATA_GROUP_ATTRS {
+  MetadataNames = "metadata_names",
+  Id = "metadata_id",
+  ImageDataIds = "image_data_ids",
+  Kinds = "kinds",
+  Planes = "planes",
+  Channels = "channels",
+  Width = "width",
+  Height = "height",
+  BitDepth = "bit_depth",
+  DefaultImageId = "defaultImageId",
+  TimeSeries = "tmeseries",
+}
+
+export enum IMAGE_GROUP_ATTRS {
+  ImageNames = "image_names",
+  Id = "image_id",
+  MetadataId = "metadata_id",
+  ClassifierPartition = "classifier_partition",
+  Timepoint = "timepoint",
+  ActivePlane = "active_plane",
+  ClassCategoryId = "class_category_id",
+}
+
+export enum ANNOTATION_GROUP_ATTRS {
+  AnnotationNames = "annotation_names",
+  Id = "annotation_id",
+  ActivePlane = "active_plane",
+  ClassCategoryId = "class_category_id",
+  ClassifierPartition = "classifier_partition",
+  Kind = "kind",
+  Bbox = "bbox",
+  Mask = "mask",
+  ImageId = "image_id",
+  Plane = "plane",
+  Timepoint = "timepoint",
+}
+
+export enum CATEGORY_GROUP_ATTRS {
+  CategoryId = "category_id",
+  Color = "color",
+  Name = "name",
+  Kind = "kind",
+}
+
+export enum KIND_GROUP_ATTRS {
+  KindId = "kind_id",
+  UnknownCategoryId = "unknown_category_id",
+  DisplayName = "display_name",
+}
+
+export enum CLASSIFIER_GROUP_ATTRS {
+  ClassifierKinds = "classifier_kinds",
+  Models = "models",
+  Name = "name",
+}
+
+export enum SEGMENTER_GROUP_ATTRS {
+  Name = "name",
 }
